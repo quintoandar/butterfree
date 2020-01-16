c376ff5814c83fb6363f337e9101b2dfa155949b
Igor Gustavo Hoelscher <ihoelscher@users.noreply.github.com>
2020-01-16 14:09:30 -0300

    [MLOP-30] Create a OnlineFeatureStoreLoader (#6)

---
+++ tests/unit/core/loader/conftest.py
@@ -0,0 +1,39 @@
+from pyspark import SparkContext
+from pyspark.sql import session
+from pytest import fixture
+
+from butterfree.core.db.configs import CassandraWriteConfig
+
+
+def base_spark():
+    sc = SparkContext.getOrCreate()
+    spark = session.SparkSession(sc)
+
+    return sc, spark
+
+
+@fixture
+def feature_set_dataframe():
+    sc, spark = base_spark()
+    data = [
+        {"id": 1, "ts": 0, "feature": 100},
+        {"id": 2, "ts": 0, "feature": 200},
+        {"id": 1, "ts": 1, "feature": 110},
+        {"id": 1, "ts": 2, "feature": 120},
+    ]
+    return spark.read.json(sc.parallelize(data, 1))
+
+
+@fixture
+def latest():
+    sc, spark = base_spark()
+    data = [
+        {"id": 2, "ts": 0, "feature": 200},
+        {"id": 1, "ts": 2, "feature": 120},
+    ]
+    return spark.read.json(sc.parallelize(data, 1))
+
+
+@fixture
+def cassandra_config():
+    return CassandraWriteConfig(keyspace="test")
