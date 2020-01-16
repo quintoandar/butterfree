67063929929a502d69a8157a26ca75531ccb3d9d
Alvaro <alvaro.marques.andrade@gmail.com>
2020-01-16 18:29:30 -0300

    added classes to __init__

--- butterfree/core/transform/__init__.py
+++ butterfree/core/transform/__init__.py
@@ -1,5 +1,6 @@
 """The Transform Component of a Feature Set."""
 
+from butterfree.core.transform.feature_component import FeatureComponent
 from butterfree.core.transform.feature import Feature
 
-__all__ = ["Feature"]
+__all__ = ["FeatureComponent", "Feature"]
