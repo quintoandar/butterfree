e9b166c1087d28ea36866649ff119f4fe8a87126
Alvaro <alvaro.marques.andrade@gmail.com>
2020-01-16 18:29:17 -0300

    updated Feature class

--- butterfree/core/transform/feature.py
+++ butterfree/core/transform/feature.py
@@ -1,9 +1,10 @@
 from typing import Union, Tuple
 from parameters_validation import non_blank
 from pyspark.sql import DataFrame
+from butterfree.core.transform import FeatureComponent
 
 
-class Feature:
+class Feature(FeatureComponent):
     def __init__(
         self,
         *,
@@ -12,14 +13,24 @@ class Feature:
         origin: Union[str, Tuple[str]],
         data_type: str = None,
         description: non_blank(str),
+        transformations: FeatureComponent = None,
     ):
         self._name = (name,)
         self._alias = (alias,)
         self._origin = (origin,)
         self._description = (description,)
         self._data_type = (data_type,)
+        self._transformations = (transformations,)
+
+    def add(self, component: FeatureComponent):
+        self._children.append(component)
+        component.parent = self
+        return self
 
     def transform(self, dataframe: DataFrame):
-        if self._alias[0] is not None:
-            return dataframe.withColumnRenamed(self._name[0], self._alias[0])
-        return dataframe
+        if not self._transformations[0]:
+            if self._alias[0]:
+                return dataframe.withColumnRenamed(self._name[0], self._alias[0])
+            return dataframe
+        for transformation in self._transformations:
+            transformation.transform(dataframe)
