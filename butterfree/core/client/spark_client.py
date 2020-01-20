172cf815e825f57c9d64eac04a9222dd6fb8069d
Rafael Leiniö <34458243+rafaelleinio@users.noreply.github.com>
2020-01-20 20:29:11 -0300

    [MLOP-21] TableSource (#16)

--- butterfree/core/client/spark_client.py
+++ butterfree/core/client/spark_client.py
@@ -35,3 +35,20 @@ class SparkClient:
         if not isinstance(options, dict):
             raise ValueError("options needs to be a dict with the setup configurations")
         return self.conn.read.format(format).options(**options).load()
+
+    def read_table(self, database, table):
+        """Use the SparkSession.read interface to load a metastore table to a dataframe.
+
+        :param database: string with the name of the metastore schema
+        :param table: string with the name of the table
+        :return:
+        """
+        if not isinstance(database, str):
+            raise ValueError(
+                "database needs to be a string with the name of the metastore schema"
+            )
+        if not isinstance(table, str):
+            raise ValueError(
+                "table needs to be a string with the name of the registered table"
+            )
+        return self.conn.read.table("{}.{}".format(database, table))
