111809a3fb151d6d80aa30d728296bd3cea235a1
Alvaro <alvaro.marques.andrade@gmail.com>
2020-01-20 16:44:13 -0300

    updated __init__

--- butterfree/core/transform/__init__.py
+++ butterfree/core/transform/__init__.py
@@ -1,6 +1,6 @@
 """The Transform Component of a Feature Set."""
 
 from butterfree.core.transform.feature import Feature
-from butterfree.core.transform.feature_component import FeatureComponent
+from butterfree.core.transform.transform_component import TransformComponent
 
-__all__ = ["FeatureComponent", "Feature"]
+__all__ = ["TransformComponent", "Feature"]
