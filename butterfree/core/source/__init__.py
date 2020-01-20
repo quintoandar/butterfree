ec5c6eec43a3fd574f6817a312ab628c5fd5a6c9
Igor Hoelscher <igor.hoelscher@gmail.com>
2020-01-20 16:49:20 -0300

    add SourceSelector class

--- butterfree/core/source/__init__.py
+++ butterfree/core/source/__init__.py
@@ -1,6 +1,7 @@
 """The Source Component of a Feature Set."""
 from butterfree.core.source.file_source import FileSource
 from butterfree.core.source.source import Source
+from butterfree.core.source.source_selector import SourceSelector
 from butterfree.core.source.table_source import TableSource
 
-__all__ = ["FileSource", "Source", "TableSource"]
+__all__ = ["FileSource", "Source", "SourceSelector", "TableSource"]
---
+++ tests/unit/core/source/test_source_selector.py
@@ -0,0 +1,34 @@
+from functools import partial
+
+from pyspark.sql import DataFrame
+
+from butterfree.core.client import SparkClient
+from butterfree.core.source import SourceSelector
+
+
+def create_temp_view(dataframe: DataFrame, name):
+    dataframe.createOrReplaceTempView(name)
+
+
+class TestSourceSelector:
+    def test_construct(self, mocker, target_df):
+        # given
+        spark_client = SparkClient()
+
+        source_id = "a_source"
+        source = mocker.stub(source_id)
+        source.build = mocker.stub("build")
+        source.build.side_effect = partial(
+            create_temp_view, target_df, source_id
+        )
+
+        # when
+        source_selector = SourceSelector(
+            spark_client=spark_client,
+            sources=[source],
+            query=f"select * from {source_id}",  # noqa
+        )
+
+        result_df = source_selector.construct()
+
+        assert result_df.collect() == target_df.collect()
