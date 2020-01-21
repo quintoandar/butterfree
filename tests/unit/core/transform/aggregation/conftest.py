a6645a1d30ec70e2f0c4ae1e9672451e24112995
Alvaro <alvaro.marques.andrade@gmail.com>
2020-01-21 20:00:01 -0300

    updated mocked data

--- tests/unit/core/transform/aggregation/conftest.py
+++ tests/unit/core/transform/aggregation/conftest.py
@@ -14,18 +14,10 @@ def base_spark():
 def feature_set_dataframe():
     sc, spark = base_spark()
     data = [
-        {"id": 1, "ts": "2016-04-11 19:01:00", "feature": 100},
-        {"id": 1, "ts": "2016-04-12 19:12:00", "feature": 200},
-        {"id": 1, "ts": "2016-04-13 19:13:00", "feature": 200},
-        {"id": 1, "ts": "2016-04-14 19:24:00", "feature": 200},
-        {"id": 1, "ts": "2016-04-14 19:28:00", "feature": 200},
-        {"id": 1, "ts": "2016-04-14 19:30:00", "feature": 200},
-        {"id": 1, "ts": "2016-04-14 19:45:00", "feature": 200},
-        {"id": 1, "ts": "2016-04-14 19:48:00", "feature": 200},
-        {"id": 1, "ts": "2016-04-17 19:50:00", "feature": 200},
-        {"id": 1, "ts": "2016-04-18 19:59:00", "feature": 200},
-        {"id": 1, "ts": "2016-04-18 20:02:00", "feature": 200},
-        {"id": 1, "ts": "2016-04-18 20:15:00", "feature": 200},
+        {"id": 1, "ts": "2016-04-11 11:31:11", "feature": 200},
+        {"id": 1, "ts": "2016-04-11 11:44:12", "feature": 300},
+        {"id": 1, "ts": "2016-04-11 11:46:24", "feature": 400},
+        {"id": 1, "ts": "2016-04-11 12:03:21", "feature": 500},
     ]
     df = spark.read.json(sc.parallelize(data, 1))
     df = df.withColumn("timestamp", df.ts.cast("timestamp"))
