23866efff4a2aea58d2385589cccae5c7ba1f310
Rafael Leinio <rafael.leper@gmail.com>
2020-01-22 14:13:29 -0300

    KafkaSource

---
+++ butterfree/core/source/kafka_source.py
@@ -0,0 +1,49 @@
+"""KafkaSource entity."""
+
+from butterfree.core.source.source import Source
+
+
+class KafkaSource(Source):
+    """Source responsible for get data from a Kafka topic."""
+
+    def __init__(self, id, spark_client, connection_string, topic, topic_options=None):
+        """Instantiate KafkaSource with the required parameters.
+
+        :param id: unique string id for register the source as a view on the metastore
+        :param spark_client: spark_client object client module
+        :param connection_string: string with hosts and ports to connect. In the format:
+        host1:port,host2:port,...,host:port
+        :param topic: string with the Kafka topic name to subscribe.
+        :param topic_options: additional options for consuming from topic. See docs:
+        https://spark.apache.org/docs/latest/structured-streaming-kafka-integration.html.
+        """
+        super().__init__(id, spark_client)
+        if not isinstance(connection_string, str):
+            raise ValueError(
+                "connection_string must be a string with hosts and ports to connect"
+            )
+        if not isinstance(topic, str):
+            raise ValueError("topic must be a string with the topic name")
+        self.connection_string = connection_string
+        self.topic = topic
+        self.options = dict(
+            {
+                "kafka.bootstrap.servers": self.connection_string,
+                "subscribe": self.topic,
+            },
+            **topic_options if topic_options else {}
+        )
+
+    def consume(self):
+        """Extract data from a kafka topic.
+
+        :return: Spark dataframe
+        """
+        raw_stream_df = self.client.read(
+            format="kafka", options=self.options, stream=True
+        )
+
+        # cast key and value columns from binary to string
+        return raw_stream_df.withColumn(
+            "key", raw_stream_df["key"].cast("string")
+        ).withColumn("value", raw_stream_df["value"].cast("string"))
