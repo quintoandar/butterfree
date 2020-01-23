3f5ba24e6abc307a018f0c090b9807b53330a9f3
AlvaroMarquesAndrade <45604858+AlvaroMarquesAndrade@users.noreply.github.com>
2020-01-23 14:52:45 -0300

    [MLOP-28] Create Aggregated Feature (#12)
    * added feature class
    
    * added feature class in __init__
    
    * added transform unit tests
    
    * created FeatureComponent abstract class
    
    * updated Feature class
    
    * added classes to __init__
    
    * please the linter
    
    * added parameters-validation to requirements.txt
    
    * feature update
    
    * feature component update
    
    * added docstring to public method
    
    * created aggregation class
    
    * __init__ from agg class
    
    * aggregation mapping definition method
    
    * window definition class
    
    * created dummy test data
    
    * unit test for aggregated feature
    
    * transform refactor
    
    * raise NotImplementedError
    
    * increased test coverage
    
    * added unit tests for FeatureComponent abstract base class
    
    * remove unnecessary docstring
    
    * created agg transform
    
    * added tests
    
    * please the linter
    
    * added docstring to the __init__
    
    * please the linter
    
    * updated feature class
    
    * updated transform component
    
    * updated __init__
    
    * test update
    
    * flake8 fix
    
    * updated feature
    
    * black compliant
    
    * test
    
    * updated agg transform
    
    * updated window mapping
    
    * updated agg feature test
    
    * make black
    
    * [MLOP-21] TableSource (#16)
    
    * [MLOP-33] Implement SourceSelector Class (#15)
    
    * add SourceSelector class
    * update spark client to run a sql query using connection
    
    * [MLOP-41] Update SparkClient to write dataframes (#13)
    
    * Create spark client writer.
    
    * Change unit test and spark_client_write.
    
    * updated parameters-validation version
    
    * added new concrete method to the abstract class
    
    * renamed class
    
    * added class parameters validation with property getter and setter
    
    * updated mocked data
    
    * added more tests
    
    * deleted files
    
    * [MLOP-31] Create a Historical Feature Store Loader (#14)
    
    * Create historical feature store and unit test
    
    * Added docstring.
    
    * Include vscode in .gitignore.
    
    * Create more unit test and fix old tests.
    
    * Change the feature_set name and fix unit test.
    
    Co-authored-by: Igor Gustavo Hoelscher <ihoelscher@users.noreply.github.com>
    
    * please the linter
    
    * fix transform
    
    * fix tests
    
    * make black
    
    * please the linter
    
    * test update
    
    * [MLOP-22] KafkaSource (#17)
    
    * renaming
    
    * please the linter
    
    Co-authored-by: Rafael Leiniö <34458243+rafaelleinio@users.noreply.github.com>
    Co-authored-by: Igor Gustavo Hoelscher <ihoelscher@users.noreply.github.com>
    Co-authored-by: Mayara Moromisato <44944954+moromimay@users.noreply.github.com>

---
+++ butterfree/core/transform/aggregation/__init__.py
@@ -0,0 +1,7 @@
+"""The Aggregated Transform Component of a Feature Set."""
+
+from butterfree.core.transform.aggregation.aggregated_transform import (
+    AggregatedTransform
+)
+
+__all__ = ["AggregatedTransform"]
