dde6f5bdd7f2a38cea4a8b51814cdefc6fd22ce5
Mayara Moromi <may.alveslima@gmail.com>
2020-01-28 15:42:25 -0300

    Apply style check.

--- butterfree/core/writer/__init__.py
+++ butterfree/core/writer/__init__.py
@@ -1,7 +1,7 @@
 """Holds spark data loaders for multiple destinations."""
 
 from butterfree.core.writer.historical_feature_store_writer import (
-    HistoricalFeatureStoreWriter
+    HistoricalFeatureStoreWriter,
 )
 from butterfree.core.writer.online_feature_store_writer import OnlineFeatureStoreWriter
 
