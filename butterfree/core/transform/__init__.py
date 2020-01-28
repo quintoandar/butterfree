a1f27a70685e87f61748554e6be5935717cf763f
Rafael Leiniö <34458243+rafaelleinio@users.noreply.github.com>
2020-01-28 17:06:42 -0300

    [MLOP-61] FeatureSet (#28)

--- butterfree/core/transform/__init__.py
+++ butterfree/core/transform/__init__.py
@@ -1,6 +1,7 @@
 """The Transform Component of a Feature Set."""
 
 from butterfree.core.transform.feature import Feature
+from butterfree.core.transform.feature_set import FeatureSet
 from butterfree.core.transform.transform_component import TransformComponent
 
-__all__ = ["TransformComponent", "Feature"]
+__all__ = ["TransformComponent", "Feature", "FeatureSet"]
