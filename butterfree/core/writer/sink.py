6d4f8dd3928d813935d8c9f60b734bc5a0fd7d73
Mayara Moromi <may.alveslima@gmail.com>
2020-01-29 12:12:04 -0300

    Fix tests.

--- butterfree/core/writer/sink.py
+++ butterfree/core/writer/sink.py
@@ -14,32 +14,39 @@ class Sink:
     Attributes:
         feature_set: object processed with feature_set informations.
         writers: list of writers to run.
+        spark_client: client for spark connections with external services.
     """
 
-    def __init__(self, feature_set: FeatureSet, writers: List[Writer]):
+    def __init__(
+        self, feature_set: FeatureSet, writers: List[Writer], spark_client: SparkClient
+    ):
         self.writers = writers
         self.feature_set = feature_set
+        self.spark_client = spark_client
 
-    def validate(self, feature_set, dataframe: DataFrame):
+    def validate(self, dataframe: DataFrame):
         """Validate to load the feature set into Writers.
 
         Args:
             feature_set: object processed with feature_set informations.
             dataframe: spark dataframe containing data from a feature set.
         """
+        check = []
         for writer in self.writers:
-            writer.validate(feature_set=feature_set, dataframe=dataframe)
+            loader = writer(self.spark_client)
+            verify = loader.validate(feature_set=self.feature_set, dataframe=dataframe)
+            check.append([writer, verify])
 
-    def flush(self, dataframe: DataFrame, spark_client: SparkClient):
+        for writer, validate_result in check:
+            if validate_result is False:
+                raise ValueError("The {} load process was failed.".format(writer))
+
+    def flush(self, dataframe: DataFrame):
         """Loads the data from a feature set into the Feature Store.
 
         Args:
             dataframe: spark dataframe containing data from a feature set.
-            spark_client: ...
         """
         for writer in self.writers:
-            writer = writer(spark_client)
+            writer = writer(self.spark_client)
             writer.write(feature_set=self.feature_set, dataframe=dataframe)
-
-            if writer.validate(self.feature_set, dataframe) is False:
-                raise ValueError("The load process was failed.")
--- tests/unit/core/writer/test_sink.py
+++ tests/unit/core/writer/test_sink.py
@@ -6,43 +6,69 @@ from butterfree.core.writer import HistoricalFeatureStoreWriter, Sink
 class TestSink:
     def test_validate(self, feature_set_dataframe, mocker):
         # given
-        writer = mocker.stub("writer")
+        spark_client = mocker.stub("spark_client")
+        writer = HistoricalFeatureStoreWriter
         writer.validate = mocker.stub("validate")
         feature_set = mocker.stub("feature_set")
 
         # when
-        sink = Sink(feature_set=feature_set, writers=[writer])
-        sink.validate(feature_set=feature_set, dataframe=feature_set_dataframe)
+        sink = Sink(
+            feature_set=feature_set, writers=[writer], spark_client=spark_client
+        )
+        sink.validate(dataframe=feature_set_dataframe)
 
         # then
         writer.validate.assert_called_once()
+
+    def test_validate_false(self, feature_set_dataframe, mocker):
+        # given
+        spark_client = mocker.stub("spark_client")
+        writer = HistoricalFeatureStoreWriter
+        writer.validate = mocker.stub("validate")
+        writer.validate.return_value = False
+        feature_set = mocker.stub("feature_set")
+
+        # when
+        sink = Sink(
+            feature_set=feature_set, writers=[writer], spark_client=spark_client
+        )
+
+        # then
+        with pytest.raises(ValueError):
+            sink.validate(dataframe=feature_set_dataframe)
 
     def test_flush(self, feature_set_dataframe, mocker):
         # given
         spark_client = mocker.stub("spark_client")
         writer = HistoricalFeatureStoreWriter
         writer.write = mocker.stub("write")
-        writer.validate = mocker.stub("validate")
         feature_set = mocker.stub("feature_set")
+        feature_set.entity = "house"
         feature_set.name = "test"
 
         # when
-        sink = Sink(feature_set=feature_set, writers=[writer])
-        sink.flush(dataframe=feature_set_dataframe, spark_client=spark_client)
+        sink = Sink(
+            feature_set=feature_set, writers=[writer], spark_client=spark_client
+        )
+        sink.flush(dataframe=feature_set_dataframe)
 
         # then
         writer.write.assert_called_once()
-        writer.validate.assert_called_once()
 
-    def test_flush_with_invalid_df(self, feature_set_dataframe, mocker):
+    def test_flush_with_invalid_df(self, feature_sets, mocker):
+        # given
         spark_client = mocker.stub("spark_client")
         writer = HistoricalFeatureStoreWriter
         feature_set = mocker.stub("feature_set")
+        feature_set.entity = "house"
         feature_set.name = "test"
 
         # when
-        sink = Sink(feature_set=feature_set, writers=[writer])
+        sink = Sink(
+            feature_set=feature_set, writers=[writer], spark_client=spark_client
+        )
 
         # then
         with pytest.raises(ValueError):
-            assert sink.flush(dataframe="oi", spark_client=spark_client)
+            sink.flush(dataframe=feature_sets)
+
