7268bfe75de911ca8de280d6426b04f4bb86bcaa
Alvaro <alvaro.marques.andrade@gmail.com>
2020-01-30 17:07:57 -0300

    updated feature set pipeline unit tests

--- tests/unit/test_feature_set_pipeline.py
+++ tests/unit/test_feature_set_pipeline.py
@@ -1,5 +1,7 @@
 from unittest.mock import Mock
 
+import pytest
+
 from butterfree.core.client import SparkClient
 from butterfree.core.feature_set_pipeline import FeatureSetPipeline
 from butterfree.core.reader import FileReader, Reader, Source, TableReader
@@ -88,6 +90,7 @@ class TestFeatureSetPipeline:
     def test_run(self):
         test_pipeline = FeatureSetPipeline(
             source=Mock(
+                spec=Source,
                 spark_client=SparkClient(),
                 readers=[
                     TableReader(
@@ -100,6 +103,7 @@ class TestFeatureSetPipeline:
                 query="select * from source_a",
             ),
             feature_set=Mock(
+                spec=FeatureSet,
                 name="feature_set",
                 entity="entity",
                 description="description",
@@ -113,6 +117,7 @@ class TestFeatureSetPipeline:
                 timestamp_column="ts",
             ),
             sink=Mock(
+                spec=Sink,
                 writers=[HistoricalFeatureStoreWriter(spark_client=SparkClient())],
             ),
         )
@@ -122,3 +127,113 @@ class TestFeatureSetPipeline:
         assert test_pipeline.feature_set.construct.assert_called_once
         assert test_pipeline.sink.flush.assert_called_once
         assert test_pipeline.sink.validate.assert_called_once
+
+    def test_source(self):
+        with pytest.raises(ValueError, match="source must be a Source instance"):
+            FeatureSetPipeline(
+                source=Mock(
+                    spark_client=SparkClient(),
+                    readers=[
+                        TableReader(
+                            id="source_a",
+                            spark_client=SparkClient(),
+                            database="db",
+                            table="table",
+                        ),
+                    ],
+                    query="select * from source_a",
+                ),
+                feature_set=Mock(
+                    spec=FeatureSet,
+                    name="feature_set",
+                    entity="entity",
+                    description="description",
+                    features=[
+                        Feature(
+                            name="user_id",
+                            description="The user's Main ID or device ID",
+                        ),
+                        Feature(name="ts", description="The timestamp feature",),
+                    ],
+                    key_columns=["user_id"],
+                    timestamp_column="ts",
+                ),
+                sink=Mock(
+                    spec=Sink,
+                    writers=[HistoricalFeatureStoreWriter(spark_client=SparkClient())],
+                ),
+            )
+
+    def test_feature_set(self):
+        with pytest.raises(
+            ValueError, match="feature_set must be a FeatureSet instance"
+        ):
+            FeatureSetPipeline(
+                source=Mock(
+                    spec=Source,
+                    spark_client=SparkClient(),
+                    readers=[
+                        TableReader(
+                            id="source_a",
+                            spark_client=SparkClient(),
+                            database="db",
+                            table="table",
+                        ),
+                    ],
+                    query="select * from source_a",
+                ),
+                feature_set=Mock(
+                    name="feature_set",
+                    entity="entity",
+                    description="description",
+                    features=[
+                        Feature(
+                            name="user_id",
+                            description="The user's Main ID or device ID",
+                        ),
+                        Feature(name="ts", description="The timestamp feature",),
+                    ],
+                    key_columns=["user_id"],
+                    timestamp_column="ts",
+                ),
+                sink=Mock(
+                    spec=Sink,
+                    writers=[HistoricalFeatureStoreWriter(spark_client=SparkClient())],
+                ),
+            )
+
+    def test_sink(self):
+        with pytest.raises(ValueError, match="sink must be a Sink instance"):
+            FeatureSetPipeline(
+                source=Mock(
+                    spec=Source,
+                    spark_client=SparkClient(),
+                    readers=[
+                        TableReader(
+                            id="source_a",
+                            spark_client=SparkClient(),
+                            database="db",
+                            table="table",
+                        ),
+                    ],
+                    query="select * from source_a",
+                ),
+                feature_set=Mock(
+                    spec=FeatureSet,
+                    name="feature_set",
+                    entity="entity",
+                    description="description",
+                    features=[
+                        Feature(
+                            name="user_id",
+                            description="The user's Main ID or device ID",
+                        ),
+                        Feature(name="ts", description="The timestamp feature",),
+                    ],
+                    key_columns=["user_id"],
+                    timestamp_column="ts",
+                ),
+                sink=Mock(
+                    writers=[HistoricalFeatureStoreWriter(spark_client=SparkClient())],
+                ),
+            )
