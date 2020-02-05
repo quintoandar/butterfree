d2b97b126a80e02f096b59d2a84355c818ea94d0
AlvaroMarquesAndrade <alvaro.marques.andrade@gmail.com>
2020-02-05 14:43:01 -0300

    defined h3 resolutions enum

---
+++ butterfree/core/constant/h3_resolutions.py
@@ -0,0 +1,15 @@
+"""H3Resolution Enum Entity."""
+
+from enum import Enum
+
+
+class H3Resolution(Enum):
+    """Holds constants for h3 resolutions within Butterfree."""
+
+    H6 = 6
+    H7 = 7
+    H8 = 8
+    H9 = 9
+    H10 = 10
+    H11 = 11
+    H12 = 12
