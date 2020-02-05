3624c6dcd45121350dac2d131e3860b0b57cae05
AlvaroMarquesAndrade <alvaro.marques.andrade@gmail.com>
2020-02-05 15:37:34 -0300

    added to __init__

--- butterfree/core/transform/transformations/__init__.py
+++ butterfree/core/transform/transformations/__init__.py
@@ -8,6 +8,9 @@ from butterfree.core.transform.transformations.aggregated_transform import (
     AggregatedTransform,
 )
 from butterfree.core.transform.transformations.custom_transform import CustomTransform
+from butterfree.core.transform.transformations.sql_expression_transform import (
+    SQLExpressionTransform,
+)
 from butterfree.core.transform.transformations.h3_transform import H3HashTransform
 from butterfree.core.transform.transformations.transform_component import (
     TransformComponent,
@@ -17,5 +20,6 @@ __all__ = [
     "AggregatedTransform",
     "CustomTransform",
     "H3HashTransform",
+    "SQLExpressionTransform",
     "TransformComponent",
 ]
