48fb7aab3d8bad8e00570ec11a723ee3218949bf
AlvaroMarquesAndrade <alvaro.marques.andrade@gmail.com>
2020-02-05 14:43:28 -0300

    added h3 transform class to __init__

--- butterfree/core/transform/transformations/__init__.py
+++ butterfree/core/transform/transformations/__init__.py
@@ -8,8 +8,14 @@ from butterfree.core.transform.transformations.aggregated_transform import (
     AggregatedTransform,
 )
 from butterfree.core.transform.transformations.custom_transform import CustomTransform
+from butterfree.core.transform.transformations.h3_transform import H3Transform
 from butterfree.core.transform.transformations.transform_component import (
     TransformComponent,
 )
 
-__all__ = ["AggregatedTransform", "CustomTransform", "TransformComponent"]
+__all__ = [
+    "AggregatedTransform",
+    "CustomTransform",
+    "H3Transform",
+    "TransformComponent",
+]
