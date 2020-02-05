e7f6d7efe71a4b19ba4c8c6cb4dd6b69cb64062a
AlvaroMarquesAndrade <alvaro.marques.andrade@gmail.com>
2020-02-05 18:46:48 -0300

    added pivot mocked data

--- tests/unit/core/reader/conftest.py
+++ tests/unit/core/reader/conftest.py
@@ -22,5 +22,17 @@ def target_df(sc, spark):
 
 
 @pytest.fixture()
+def pivot_df(sc, spark):
+    data = [
+        {"id": 1, "ts": "2016-04-11 11:31:11", "pivot_column": 1, "has_feature": 1},
+        {"id": 1, "ts": "2016-04-11 11:44:12", "pivot_column": 2, "has_feature": 0},
+        {"id": 1, "ts": "2016-04-11 11:46:24", "pivot_column": 3, "has_feature": 1},
+        {"id": 1, "ts": "2016-04-11 12:03:21", "pivot_column": 4, "has_feature": 0},
+    ]
+    df = spark.read.json(sc.parallelize(data, 1))
+    return df
+
+
+@pytest.fixture()
 def spark_client():
     return Mock()
