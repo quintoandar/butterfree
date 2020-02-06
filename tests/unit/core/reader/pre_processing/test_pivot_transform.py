77867e6bed5d40edd5d082c9ed29d7d460dfa05d
AlvaroMarquesAndrade <alvaro.marques.andrade@gmail.com>
2020-02-06 18:42:51 -0300

    fix test

--- tests/unit/core/reader/pre_processing/test_pivot_transform.py
+++ tests/unit/core/reader/pre_processing/test_pivot_transform.py
@@ -21,27 +21,24 @@ class TestPivotTransform:
     def test_pivot_transformation(
         self, pivot_df, target_pivot_df,
     ):
-        df = (
-            pivot_table(
-                dataframe=pivot_df,
-                group_by_columns=["id", "ts"],
-                pivot_column="pivot_column",
-                aggregation_expression=first("has_feature"),
-            )
-            .orderBy("ts")
-            .collect()
+        result_df = pivot_table(
+            dataframe=pivot_df,
+            group_by_columns=["id", "ts"],
+            pivot_column="pivot_column",
+            aggregation_expression=first("has_feature"),
         )
 
-        target_df = target_pivot_df.collect()
+        target_df = target_pivot_df
 
         # assert
-        for line in range(0, 4):
-            assert df[line]["id"] == target_df[line]["id"]
-            assert df[line]["ts"] == target_df[line]["ts"]
-            assert df[line]["1"] == target_df[line]["1"]
-            assert df[line]["2"] == target_df[line]["2"]
-            assert df[line]["3"] == target_df[line]["3"]
-            assert df[line]["4"] == target_df[line]["4"]
+        assert (
+            compare_dataframes(
+                actual_df=result_df,
+                expected_df=target_df,
+                columns_sort=result_df.columns,
+            )
+            is True
+        )
 
     def test_apply_pivot_transformation(self, pivot_df, target_pivot_df):
         # arrange
