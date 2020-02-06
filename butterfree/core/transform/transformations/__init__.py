3422fe064f773f6dfff870bbb8963e0e2f90d0e0
AlvaroMarquesAndrade <alvaro.marques.andrade@gmail.com>
2020-02-06 14:14:47 -0300

    isort compliant

--- butterfree/core/transform/transformations/__init__.py
+++ butterfree/core/transform/transformations/__init__.py
@@ -8,10 +8,10 @@ from butterfree.core.transform.transformations.aggregated_transform import (
     AggregatedTransform,
 )
 from butterfree.core.transform.transformations.custom_transform import CustomTransform
+from butterfree.core.transform.transformations.h3_transform import H3HashTransform
 from butterfree.core.transform.transformations.sql_expression_transform import (
     SQLExpressionTransform,
 )
-from butterfree.core.transform.transformations.h3_transform import H3HashTransform
 from butterfree.core.transform.transformations.transform_component import (
     TransformComponent,
 )
