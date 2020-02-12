66a26315078b31fb0d1f229113077392becbe58f
AlvaroMarquesAndrade <alvaro.marques.andrade@gmail.com>
2020-02-12 19:28:26 -0300

    added mocked data

--- tests/unit/butterfree/core/transform/features/conftest.py
+++ tests/unit/butterfree/core/transform/features/conftest.py
@@ -27,5 +27,35 @@ def feature_set_dataframe(base_spark):
 
 
 @fixture
+def feature_set_dataframe_ms_from_column(base_spark):
+    sc, spark = base_spark
+    data = [
+        {"id": 1, "ts": 1581542311000, "feature": 100},
+        {"id": 2, "ts": 1581542322000, "feature": 200},
+    ]
+    return spark.read.json(sc.parallelize(data, 1))
+
+
+@fixture
+def feature_set_dataframe_ms(base_spark):
+    sc, spark = base_spark
+    data = [
+        {"id": 1, TIMESTAMP_COLUMN: 1581542311000, "feature": 100},
+        {"id": 2, TIMESTAMP_COLUMN: 1581542322000, "feature": 200},
+    ]
+    return spark.read.json(sc.parallelize(data, 1))
+
+
+@fixture
+def feature_set_dataframe_date(base_spark):
+    sc, spark = base_spark
+    data = [
+        {"id": 1, TIMESTAMP_COLUMN: "2020-02-07 00:00:00", "feature": 100},
+        {"id": 2, TIMESTAMP_COLUMN: "2020-02-08 00:00:00", "feature": 200},
+    ]
+    return spark.read.json(sc.parallelize(data, 1))
+
+
+@fixture
 def mocked_feature():
     return Mock(spec=Feature)
