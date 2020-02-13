d06678154749af20aea2edbcf9cec826e2f95592
AlvaroMarquesAndrade <alvaro.marques.andrade@gmail.com>
2020-02-13 19:10:08 -0300

    please the linter

--- tests/unit/core/transform/transformations/test_aggregated_transform.py
+++ tests/unit/core/transform/transformations/test_aggregated_transform.py
@@ -191,9 +191,7 @@ class TestAggregatedTransform:
         )
 
         df = (
-            test_feature.transform(feature_set_dataframe)
-            .orderBy("timestamp")
-            .collect()
+            test_feature.transform(feature_set_dataframe).orderBy("timestamp").collect()
         )
 
         assert df[0]["feature1__avg_over_1_day_rolling_windows"] == 350
@@ -202,7 +200,10 @@ class TestAggregatedTransform:
         assert df[3]["feature1__avg_over_1_day_rolling_windows"] is None
         assert df[4]["feature1__avg_over_1_day_rolling_windows"] is None
         assert df[5]["feature1__avg_over_1_day_rolling_windows"] is None
-        assert df[0]["feature1__stddev_pop_over_1_day_rolling_windows"] == 111.80339887498948
+        assert (
+            df[0]["feature1__stddev_pop_over_1_day_rolling_windows"]
+            == 111.80339887498948
+        )
         assert df[1]["feature1__stddev_pop_over_1_day_rolling_windows"] is None
         assert df[2]["feature1__stddev_pop_over_1_day_rolling_windows"] is None
         assert df[3]["feature1__stddev_pop_over_1_day_rolling_windows"] is None
@@ -214,8 +215,14 @@ class TestAggregatedTransform:
         assert df[3]["feature1__avg_over_1_week_rolling_windows"] == 350
         assert df[4]["feature1__avg_over_1_week_rolling_windows"] == 350
         assert df[5]["feature1__avg_over_1_week_rolling_windows"] == 350
-        assert df[0]["feature1__stddev_pop_over_1_week_rolling_windows"] == 111.80339887498948
-        assert df[1]["feature1__stddev_pop_over_1_week_rolling_windows"] == 111.80339887498948
+        assert (
+            df[0]["feature1__stddev_pop_over_1_week_rolling_windows"]
+            == 111.80339887498948
+        )
+        assert (
+            df[1]["feature1__stddev_pop_over_1_week_rolling_windows"]
+            == 111.80339887498948
+        )
         assert (
             df[2]["feature1__stddev_pop_over_1_week_rolling_windows"]
             == 111.80339887498948
@@ -224,8 +231,14 @@ class TestAggregatedTransform:
             df[3]["feature1__stddev_pop_over_1_week_rolling_windows"]
             == 111.80339887498948
         )
-        assert df[4]["feature1__stddev_pop_over_1_week_rolling_windows"] == 111.80339887498948
-        assert df[5]["feature1__stddev_pop_over_1_week_rolling_windows"] == 111.80339887498948
+        assert (
+            df[4]["feature1__stddev_pop_over_1_week_rolling_windows"]
+            == 111.80339887498948
+        )
+        assert (
+            df[5]["feature1__stddev_pop_over_1_week_rolling_windows"]
+            == 111.80339887498948
+        )
 
     def test_feature_transform_empty_mode(self, feature_set_dataframe):
         with pytest.raises(ValueError, match="Modes must not be empty."):
