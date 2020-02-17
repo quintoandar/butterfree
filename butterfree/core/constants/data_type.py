7a8a24d06c673a682a645b7e4733599557bc08fd
Mayara Moromisato <may.alveslima@gmail.com>
2020-02-17 10:52:03 -0300

    Add more DataType to ENUM.

--- butterfree/core/constants/data_type.py
+++ butterfree/core/constants/data_type.py
@@ -2,10 +2,21 @@
 
 from enum import Enum
 
-from pyspark.sql.types import TimestampType
+from pyspark.sql.types import TimestampType, BinaryType, BooleanType, ByteType, DateType, DecimalType, DoubleType, FloatType, IntegerType, LongType, ShortType, StringType
 
 
 class DataType(Enum):
     """Holds constants for data types within Butterfree."""
 
     TIMESTAMP = TimestampType()
+    BINARY = BinaryType()
+    BOOLEAN = BooleanType()
+    BYTE = ByteType()
+    DATE = DateType()
+    DECIMAL = DecimalType()
+    DOUBLE = DoubleType()
+    FLOAT = FloatType()
+    INTEGER = IntegerType()
+    BIGINT = LongType()
+    SMALLINT = ShortType()
+    STRING = StringType()
