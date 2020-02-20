42c2e81f926fc6cf28bd7639b3bf445c55bb1f2f
Igor Gustavo Hoelscher <ihoelscher@users.noreply.github.com>
2020-02-20 14:14:58 -0300

    [MLOP-133] Fix pivoting without traceback (#65)
    * add forward fill transform
    * adapt pivot to use forward fill

--- tests/unit/butterfree/core/extract/pre_processing/conftest.py
+++ tests/unit/butterfree/core/extract/pre_processing/conftest.py
@@ -1,7 +1,9 @@
+import json
+from typing import List
+
 import pytest
 from pyspark import SparkContext
-from pyspark.sql import session
-from pyspark.sql.functions import col, when
+from pyspark.sql import DataFrame, session
 
 
 @pytest.fixture()
@@ -19,26 +21,69 @@ def spark(sc):
 
 
 @pytest.fixture()
-def pivot_df(sc, spark):
+def input_df(sc, spark):
     data = [
         {"id": 1, "ts": "2016-04-11 11:31:11", "pivot_column": 1, "has_feature": 1},
         {"id": 1, "ts": "2016-04-11 11:44:12", "pivot_column": 2, "has_feature": 0},
         {"id": 1, "ts": "2016-04-11 11:46:24", "pivot_column": 3, "has_feature": 1},
         {"id": 1, "ts": "2016-04-11 12:03:21", "pivot_column": 4, "has_feature": 0},
+        {"id": 1, "ts": "2016-04-11 13:46:24", "pivot_column": 3, "has_feature": None},
     ]
-    df = spark.read.json(sc.parallelize(data, 1))
+    df = spark.read.json(sc.parallelize(data).map(lambda x: json.dumps(x)))
     return df
 
 
 @pytest.fixture()
-def target_pivot_df(sc, spark):
+def pivot_df(sc, spark):
     data = [
-        {"id": 1, "ts": "2016-04-11 11:31:11", "1": 1, "2": "", "3": "", "4": ""},
-        {"id": 1, "ts": "2016-04-11 11:44:12", "1": "", "2": 0, "3": "", "4": ""},
-        {"id": 1, "ts": "2016-04-11 11:46:24", "1": "", "2": "", "3": 1, "4": ""},
-        {"id": 1, "ts": "2016-04-11 12:03:21", "1": "", "2": "", "3": "", "4": 0},
+        {"id": 1, "ts": "2016-04-11 11:31:11", "1": 1, "2": None, "3": None, "4": None},
+        {"id": 1, "ts": "2016-04-11 11:44:12", "1": None, "2": 0, "3": None, "4": None},
+        {"id": 1, "ts": "2016-04-11 11:46:24", "1": None, "2": None, "3": 1, "4": None},
+        {"id": 1, "ts": "2016-04-11 12:03:21", "1": None, "2": None, "3": None, "4": 0},
+        {
+            "id": 1,
+            "ts": "2016-04-11 13:46:24",
+            "1": None,
+            "2": None,
+            "3": None,
+            "4": None,
+        },
     ]
-    df = spark.read.json(sc.parallelize(data, 1))
-    for column in df.columns:
-        df = df.withColumn(column, when(col(column) == "", None).otherwise(col(column)))
+    df = spark.read.json(sc.parallelize(data).map(lambda x: json.dumps(x)))
     return df.orderBy("ts")
+
+
+@pytest.fixture()
+def pivot_ffill_df(sc, spark):
+    data = [
+        {"id": 1, "ts": "2016-04-11 11:31:11", "1": 1, "2": None, "3": None, "4": None},
+        {"id": 1, "ts": "2016-04-11 11:44:12", "1": 1, "2": 0, "3": None, "4": None},
+        {"id": 1, "ts": "2016-04-11 11:46:24", "1": 1, "2": 0, "3": 1, "4": None},
+        {"id": 1, "ts": "2016-04-11 12:03:21", "1": 1, "2": 0, "3": 1, "4": 0},
+        {"id": 1, "ts": "2016-04-11 13:46:24", "1": 1, "2": 0, "3": 1, "4": 0},
+    ]
+    df = spark.read.json(sc.parallelize(data).map(lambda x: json.dumps(x)))
+    return df.orderBy("ts")
+
+
+@pytest.fixture()
+def pivot_ffill_mock_df(sc, spark):
+    data = [
+        {"id": 1, "ts": "2016-04-11 11:31:11", "1": 1, "2": None, "3": None, "4": None},
+        {"id": 1, "ts": "2016-04-11 11:44:12", "1": 1, "2": 0, "3": None, "4": None},
+        {"id": 1, "ts": "2016-04-11 11:46:24", "1": 1, "2": 0, "3": 1, "4": None},
+        {"id": 1, "ts": "2016-04-11 12:03:21", "1": 1, "2": 0, "3": 1, "4": 0},
+        {"id": 1, "ts": "2016-04-11 13:46:24", "1": 1, "2": 0, "3": None, "4": 0},
+    ]
+    df = spark.read.json(sc.parallelize(data).map(lambda x: json.dumps(x)))
+    return df.orderBy("ts")
+
+
+def compare_dataframes(
+    actual_df: DataFrame, expected_df: DataFrame, columns_sort: List[str] = None
+):
+    if not columns_sort:
+        columns_sort = actual_df.schema.fieldNames()
+    return sorted(actual_df.select(*columns_sort).collect()) == sorted(
+        expected_df.select(*columns_sort).collect()
+    )
