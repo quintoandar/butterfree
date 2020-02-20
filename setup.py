465882e5608e0016705264b0fc266153b3ebae1e
Mayara Moromisato <44944954+moromimay@users.noreply.github.com>
2020-02-20 09:49:32 -0300

    [MLOP-143] Fix Bugs for HouseMain FeatureSet (#62)
    * Add more DataType to ENUM.
    
    * Add repartition to create_partitions method.

--- setup.py
+++ setup.py
@@ -27,5 +27,5 @@ setup(
     description="This repository contains ETL scripts defining the feature sets",
     keywords="feature store sets ETL",
     install_requires=requirements,
-    extras_require={"h3": ["cmake==3.13.3", "h3==3.4.3"]},
+    extras_require={"h3": ["cmake==3.16.3", "h3==3.4.2"]},
 )
