33d500c39527b510309981cba84a55e0ac25b074
AlvaroMarquesAndrade <alvaro.marques.andrade@gmail.com>
2020-02-21 16:04:15 -0300

    updated conftest

--- tests/unit/butterfree/core/extract/pre_processing/conftest.py
+++ tests/unit/butterfree/core/extract/pre_processing/conftest.py
@@ -1,9 +1,12 @@
+import json
+from typing import List
+
 import pytest
-from pyspark.sql.functions import col, when
+from pyspark.sql import DataFrame
 
 
 @pytest.fixture()
-def pivot_df(spark_context, spark_session):
+def input_df(spark_context, spark_session):
     data = [
         {"id": 1, "ts": "2016-04-11 11:31:11", "pivot_column": 1, "has_feature": 1},
         {"id": 1, "ts": "2016-04-11 11:44:12", "pivot_column": 2, "has_feature": 0},
@@ -11,12 +14,45 @@ def pivot_df(spark_context, spark_session):
         {"id": 1, "ts": "2016-04-11 12:03:21", "pivot_column": 4, "has_feature": 0},
         {"id": 1, "ts": "2016-04-11 13:46:24", "pivot_column": 3, "has_feature": None},
     ]
-    df = spark_session.read.json(spark_context.parallelize(data, 1))
+    df = spark_session.read.json(spark_context.parallelize(data).map(lambda x: json.dumps(x)))
     return df
 
 
 @pytest.fixture()
-def target_pivot_df(spark_context, spark_session):
+def pivot_df(spark_context, spark_session):
+    data = [
+        {"id": 1, "ts": "2016-04-11 11:31:11", "1": 1, "2": None, "3": None, "4": None},
+        {"id": 1, "ts": "2016-04-11 11:44:12", "1": None, "2": 0, "3": None, "4": None},
+        {"id": 1, "ts": "2016-04-11 11:46:24", "1": None, "2": None, "3": 1, "4": None},
+        {"id": 1, "ts": "2016-04-11 12:03:21", "1": None, "2": None, "3": None, "4": 0},
+        {
+            "id": 1,
+            "ts": "2016-04-11 13:46:24",
+            "1": None,
+            "2": None,
+            "3": None,
+            "4": None,
+        },
+    ]
+    df = spark_session.read.json(spark_context.parallelize(data).map(lambda x: json.dumps(x)))
+    return df.orderBy("ts")
+
+
+@pytest.fixture()
+def pivot_ffill_df(spark_context, spark_session):
+    data = [
+        {"id": 1, "ts": "2016-04-11 11:31:11", "1": 1, "2": None, "3": None, "4": None},
+        {"id": 1, "ts": "2016-04-11 11:44:12", "1": 1, "2": 0, "3": None, "4": None},
+        {"id": 1, "ts": "2016-04-11 11:46:24", "1": 1, "2": 0, "3": 1, "4": None},
+        {"id": 1, "ts": "2016-04-11 12:03:21", "1": 1, "2": 0, "3": 1, "4": 0},
+        {"id": 1, "ts": "2016-04-11 13:46:24", "1": 1, "2": 0, "3": 1, "4": 0},
+    ]
+    df = spark_session.read.json(spark_context.parallelize(data).map(lambda x: json.dumps(x)))
+    return df.orderBy("ts")
+
+
+@pytest.fixture()
+def pivot_ffill_mock_df(spark_context, spark_session):
     data = [
         {"id": 1, "ts": "2016-04-11 11:31:11", "1": 1, "2": None, "3": None, "4": None},
         {"id": 1, "ts": "2016-04-11 11:44:12", "1": 1, "2": 0, "3": None, "4": None},
@@ -24,9 +60,7 @@ def target_pivot_df(spark_context, spark_session):
         {"id": 1, "ts": "2016-04-11 12:03:21", "1": 1, "2": 0, "3": 1, "4": 0},
         {"id": 1, "ts": "2016-04-11 13:46:24", "1": 1, "2": 0, "3": None, "4": 0},
     ]
-    df = spark_session.read.json(spark_context.parallelize(data, 1))
-    for column in df.columns:
-        df = df.withColumn(column, when(col(column) == "", None).otherwise(col(column)))
+    df = spark_session.read.json(spark_context.parallelize(data).map(lambda x: json.dumps(x)))
     return df.orderBy("ts")
 
 
