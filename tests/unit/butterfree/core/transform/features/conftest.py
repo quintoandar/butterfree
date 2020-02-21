a30ce7b3fb8c8fb076f5a8519b524f8da7ce25af
AlvaroMarquesAndrade <45604858+AlvaroMarquesAndrade@users.noreply.github.com>
2020-02-21 16:17:32 -0300

    [MLOP-149] Use pytest-spark fixtures (#64)
    * added pytest-spark to requirements.test
    
    * updated clients conftest
    
    * updated pre_processing conftest
    
    * updated filter transform unit test
    
    * updated kafka reader unit test
    
    * updated readers conftest
    
    * updated source conftest
    
    * updated sink conftest
    
    * updated features conftest
    
    * updated transformations conftest
    
    * updated validations conftest
    
    * updated feature_set conftest
    
    * updated source conftest
    
    * updated source integration test
    
    * updated sink conftest
    
    * updated feature set pipeline conftest
    
    * updated feature set pipeline integration test
    
    * updated feature set conftest
    
    * removed spark config from features conftest
    
    * removed spark config from pre_processing conftest
    
    * added default spark config to timeZone
    
    * updated conftest
    
    * please the linter

--- tests/unit/butterfree/core/transform/features/conftest.py
+++ tests/unit/butterfree/core/transform/features/conftest.py
@@ -1,7 +1,5 @@
 from unittest.mock import Mock
 
-from pyspark import SparkContext
-from pyspark.sql import session
 from pytest import fixture
 
 from butterfree.core.constants.columns import TIMESTAMP_COLUMN
@@ -9,55 +7,39 @@ from butterfree.core.transform.features import Feature
 
 
 @fixture
-def base_spark():
-    sc = SparkContext.getOrCreate()
-    spark = (
-        session.SparkSession(sc)
-        .builder.config("spark.sql.session.timeZone", "UTC")
-        .getOrCreate()
-    )
-
-    return sc, spark
-
-
-@fixture
-def feature_set_dataframe(base_spark):
-    sc, spark = base_spark
+def feature_set_dataframe(spark_context, spark_session):
     data = [
         {"id": 1, TIMESTAMP_COLUMN: 0, "feature": 100},
         {"id": 2, TIMESTAMP_COLUMN: 1, "feature": 200},
     ]
-    return spark.read.json(sc.parallelize(data, 1))
+    return spark_session.read.json(spark_context.parallelize(data, 1))
 
 
 @fixture
-def feature_set_dataframe_ms_from_column(base_spark):
-    sc, spark = base_spark
+def feature_set_dataframe_ms_from_column(spark_context, spark_session):
     data = [
         {"id": 1, "ts": 1581542311000, "feature": 100},
         {"id": 2, "ts": 1581542322000, "feature": 200},
     ]
-    return spark.read.json(sc.parallelize(data, 1))
+    return spark_session.read.json(spark_context.parallelize(data, 1))
 
 
 @fixture
-def feature_set_dataframe_ms(base_spark):
-    sc, spark = base_spark
+def feature_set_dataframe_ms(spark_context, spark_session):
     data = [
         {"id": 1, TIMESTAMP_COLUMN: 1581542311000, "feature": 100},
         {"id": 2, TIMESTAMP_COLUMN: 1581542322000, "feature": 200},
     ]
-    return spark.read.json(sc.parallelize(data, 1))
+    return spark_session.read.json(spark_context.parallelize(data, 1))
 
 
 @fixture
-def feature_set_dataframe_date(base_spark):
-    sc, spark = base_spark
+def feature_set_dataframe_date(spark_context, spark_session):
     data = [
         {"id": 1, TIMESTAMP_COLUMN: "2020-02-07 00:00:00", "feature": 100},
         {"id": 2, TIMESTAMP_COLUMN: "2020-02-08 00:00:00", "feature": 200},
     ]
-    return spark.read.json(sc.parallelize(data, 1))
+    return spark_session.read.json(spark_context.parallelize(data, 1))
 
 
 @fixture
