414e194c53181faf681cccb159b8867b975d0db4
AlvaroMarquesAndrade <45604858+AlvaroMarquesAndrade@users.noreply.github.com>
2020-02-21 16:17:32 -0300

    [MLOP-149] Use pytest-spark fixtures (#64)
    * added pytest-spark to requirements.test
    
    * updated clients conftest
    
    * updated pre_processing conftest
    
    * updated filter transform unit test
    
    * updated kafka reader unit test
    
    * updated readers conftest
    
    * updated source conftest
    
    * updated sink conftest
    
    * updated features conftest
    
    * updated transformations conftest
    
    * updated validations conftest
    
    * updated feature_set conftest
    
    * updated source conftest
    
    * updated source integration test
    
    * updated sink conftest
    
    * updated feature set pipeline conftest
    
    * updated feature set pipeline integration test
    
    * updated feature set conftest
    
    * removed spark config from features conftest
    
    * removed spark config from pre_processing conftest
    
    * added default spark config to timeZone
    
    * updated conftest
    
    * please the linter

--- tests/integration/butterfree/core/transform/conftest.py
+++ tests/integration/butterfree/core/transform/conftest.py
@@ -1,28 +1,18 @@
-from pyspark import SparkContext
-from pyspark.sql import session
 from pytest import fixture
 
 from butterfree.core.constants.columns import TIMESTAMP_COLUMN
 from butterfree.core.constants.data_type import DataType
 
 
-def base_spark():
-    sc = SparkContext.getOrCreate()
-    spark = session.SparkSession(sc)
-
-    return sc, spark
-
-
 @fixture
-def feature_set_dataframe():
-    sc, spark = base_spark()
+def feature_set_dataframe(spark_context, spark_session):
     data = [
         {"id": 1, "origin_ts": "2016-04-11 11:31:11", "feature1": 200, "feature2": 200},
         {"id": 1, "origin_ts": "2016-04-11 11:44:12", "feature1": 300, "feature2": 300},
         {"id": 1, "origin_ts": "2016-04-11 11:46:24", "feature1": 400, "feature2": 400},
         {"id": 1, "origin_ts": "2016-04-11 12:03:21", "feature1": 500, "feature2": 500},
     ]
-    df = spark.read.json(sc.parallelize(data, 1))
+    df = spark_session.read.json(spark_context.parallelize(data, 1))
     df = df.withColumn(TIMESTAMP_COLUMN, df.origin_ts.cast(DataType.TIMESTAMP.value))
 
     return df
