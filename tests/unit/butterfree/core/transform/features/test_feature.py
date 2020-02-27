51954a35a137d6031ea7646aae8e39fe52a44054
AlvaroMarquesAndrade <alvaro.marques.andrade@gmail.com>
2020-02-27 20:34:21 -0300

    refactor feature unit test

--- tests/unit/butterfree/core/transform/features/test_feature.py
+++ tests/unit/butterfree/core/transform/features/test_feature.py
@@ -29,84 +29,65 @@ class TestFeature:
         assert test_feature.description == "unit test"
         assert test_feature.transformation
 
-    def test_feature_transform_no_from_column(self, feature_set_dataframe):
+    def test_feature_transform_no_from_column(self, target_df):
 
         test_feature = Feature(
             name="feature", description="unit test feature without transformation",
         )
 
-        df = test_feature.transform(feature_set_dataframe)
+        output_df = test_feature.transform(target_df)
 
-        assert all([a == b for a, b in zip(df.columns, feature_set_dataframe.columns)])
+        assert sorted(output_df.columns) == sorted(target_df.columns)
 
-    def test_feature_transform_with_from_column(self, feature_set_dataframe):
-
-        test_feature = Feature(
-            name="new_feature", from_column="feature", description="unit test",
-        )
-        df = test_feature.transform(feature_set_dataframe)
-
-        assert all(
-            [
-                a == b
-                for a, b in zip(
-                    sorted(df.columns), sorted(["new_feature", "id", TIMESTAMP_COLUMN])
-                )
-            ]
-        )
-
-    def test_feature_transform_with_dtype(self, feature_set_dataframe):
+    def test_feature_transform_with_from_column(self, target_df):
 
         test_feature = Feature(
-            name="feature", description="unit test", dtype=DataType.TIMESTAMP
+            name="new_feature1", from_column="feature1", description="unit test",
         )
-        df = test_feature.transform(feature_set_dataframe)
+        output_df = test_feature.transform(target_df)
 
-        assert dict(df.dtypes).get("feature") == "timestamp"
+        assert sorted(output_df.columns) == sorted(
+            ["new_feature1", "feature2", "id", TIMESTAMP_COLUMN]
+        )
 
-    def test_feature_transform_with_transformation_no_from_column(
-        self, feature_set_dataframe
-    ):
+    def test_feature_transform_with_dtype(self, target_df):
+
+        test_feature = Feature(
+            name="feature1", description="unit test", dtype=DataType.TIMESTAMP
+        )
+        output_df = test_feature.transform(target_df)
+
+        assert dict(output_df.dtypes).get("feature1") == "timestamp"
+
+    def test_feature_transform_with_transformation_no_from_column(self, target_df):
         some_transformation = Mock()
-        some_transformation.transform.return_value = feature_set_dataframe
+        some_transformation.transform.return_value = target_df
 
         test_feature = Feature(
             name="feature", description="unit test", transformation=some_transformation,
         )
 
-        df = test_feature.transform(feature_set_dataframe)
+        output_df = test_feature.transform(target_df)
 
-        assert all(
-            [
-                a == b
-                for a, b in zip(
-                    sorted(df.columns), sorted(["feature", "id", TIMESTAMP_COLUMN])
-                )
-            ]
+        assert sorted(output_df.columns) == sorted(
+            ["feature1", "feature2", "id", TIMESTAMP_COLUMN]
         )
 
-    def test_feature_transform_with_transformation_and_alias(
-        self, feature_set_dataframe
-    ):
+    def test_feature_transform_with_transformation_and_alias(self, target_df):
         some_transformation = Mock()
-        some_transformation.transform.return_value = feature_set_dataframe
+        some_transformation.transform.return_value = target_df
 
         test_feature = Feature(
-            name="feature",
+            name="feature1",
             from_column="origin",
             description="unit test",
             transformation=some_transformation,
         )
 
-        df = test_feature.transform(feature_set_dataframe)
+        output_df = test_feature.transform(target_df)
 
-        assert all(
-            [
-                a == b
-                for a, b in zip(
-                    sorted(df.columns), sorted(["feature", "id", TIMESTAMP_COLUMN])
-                )
-            ]
+        assert sorted(output_df.columns) == sorted(
+            ["feature1", "feature2", "id", TIMESTAMP_COLUMN]
         )
 
     def test_feature_get_output_columns_without_transformations(self):
@@ -117,12 +98,10 @@ class TestFeature:
 
         assert test_feature.get_output_columns() == [test_feature.name]
 
-    def test_feature_get_output_columns_with_transformations(
-        self, feature_set_dataframe
-    ):
+    def test_feature_get_output_columns_with_transformations(self, target_df):
 
         some_transformation = Mock()
-        some_transformation.output_columns = feature_set_dataframe.columns
+        some_transformation.output_columns = target_df.columns
 
         test_feature = Feature(
             name="feature",
@@ -131,4 +110,4 @@ class TestFeature:
             transformation=some_transformation,
         )
 
-        assert test_feature.get_output_columns() == feature_set_dataframe.columns
+        assert test_feature.get_output_columns() == target_df.columns
