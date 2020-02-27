b28271aa7d469a5e0ef1017fae0285e757e2d91b
AlvaroMarquesAndrade <alvaro.marques.andrade@gmail.com>
2020-02-27 20:34:10 -0300

    refactor features mocked data

--- tests/unit/butterfree/core/transform/features/conftest.py
+++ tests/unit/butterfree/core/transform/features/conftest.py
@@ -1,47 +1,46 @@
-from unittest.mock import Mock
-
 from pytest import fixture
 
 from butterfree.core.constants.columns import TIMESTAMP_COLUMN
-from butterfree.core.transform.features import Feature
+from butterfree.core.constants.data_type import DataType
 
 
 @fixture
-def feature_set_dataframe(spark_context, spark_session):
+def input_ms_from_column(spark_context, spark_session):
     data = [
-        {"id": 1, TIMESTAMP_COLUMN: 0, "feature": 100},
-        {"id": 2, TIMESTAMP_COLUMN: 1, "feature": 200},
+        {"id": 1, "ts": 1460374271000, "feature1": 100, "feature2": 100},
+        {"id": 2, "ts": 1460375052000, "feature1": 200, "feature2": 200},
+        {"id": 3, "ts": 1460375184000, "feature1": 300, "feature2": 300},
+        {"id": 4, "ts": 1460376201000, "feature1": 400, "feature2": 400},
     ]
     return spark_session.read.json(spark_context.parallelize(data, 1))
 
 
 @fixture
-def feature_set_dataframe_ms_from_column(spark_context, spark_session):
+def input_ms(spark_context, spark_session):
     data = [
-        {"id": 1, "ts": 1581542311000, "feature": 100},
-        {"id": 2, "ts": 1581542322000, "feature": 200},
+        {"id": 1, TIMESTAMP_COLUMN: 1460374271000, "feature1": 100, "feature2": 100},
+        {"id": 2, TIMESTAMP_COLUMN: 1460375052000, "feature1": 200, "feature2": 200},
+        {"id": 3, TIMESTAMP_COLUMN: 1460375184000, "feature1": 300, "feature2": 300},
+        {"id": 4, TIMESTAMP_COLUMN: 1460376201000, "feature1": 400, "feature2": 400},
     ]
     return spark_session.read.json(spark_context.parallelize(data, 1))
 
 
 @fixture
-def feature_set_dataframe_ms(spark_context, spark_session):
+def input_date(spark_context, spark_session):
     data = [
-        {"id": 1, TIMESTAMP_COLUMN: 1581542311000, "feature": 100},
-        {"id": 2, TIMESTAMP_COLUMN: 1581542322000, "feature": 200},
+        {"id": 1, TIMESTAMP_COLUMN: "2019-02-12", "feature": 100},
+        {"id": 2, TIMESTAMP_COLUMN: "2019-02-12", "feature": 200},
     ]
     return spark_session.read.json(spark_context.parallelize(data, 1))
 
 
 @fixture
-def feature_set_dataframe_date(spark_context, spark_session):
+def date_target_df(spark_context, spark_session):
     data = [
-        {"id": 1, TIMESTAMP_COLUMN: "2020-02-07 00:00:00", "feature": 100},
-        {"id": 2, TIMESTAMP_COLUMN: "2020-02-08 00:00:00", "feature": 200},
+        {"id": 1, TIMESTAMP_COLUMN: "2019-02-12 00:00:00", "feature": 100},
+        {"id": 2, TIMESTAMP_COLUMN: "2019-02-12 00:00:00", "feature": 200},
     ]
-    return spark_session.read.json(spark_context.parallelize(data, 1))
-
-
-@fixture
-def mocked_feature():
-    return Mock(spec=Feature)
+    df = spark_session.read.json(spark_context.parallelize(data, 1))
+    df = df.withColumn(TIMESTAMP_COLUMN, df.timestamp.cast(DataType.TIMESTAMP.value))
+    return df
