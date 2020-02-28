55ac20614a197c59abff6c33a9d7970d67349b7e
AlvaroMarquesAndrade <alvaro.marques.andrade@gmail.com>
2020-02-28 13:46:22 -0300

    added new mocked data

--- tests/unit/butterfree/core/conftest.py
+++ tests/unit/butterfree/core/conftest.py
@@ -1,3 +1,5 @@
+import json
+
 from pyspark.sql.types import StringType, StructField, StructType
 from pytest import fixture
 
@@ -13,7 +15,9 @@ def target_df(spark_context, spark_session):
         {"id": 3, TIMESTAMP_COLUMN: 1460375184, "feature1": 300, "feature2": 300},
         {"id": 4, TIMESTAMP_COLUMN: 1460376201, "feature1": 400, "feature2": 400},
     ]
-    df = spark_session.read.json(spark_context.parallelize(data, 1))
+    df = spark_session.read.json(
+        spark_context.parallelize(data).map(lambda x: json.dumps(x))
+    )
     df = df.withColumn(TIMESTAMP_COLUMN, df.timestamp.cast(DataType.TIMESTAMP.value))
     return df
 
@@ -26,7 +30,9 @@ def target_df_without_ts(spark_context, spark_session):
         {"id": 3, "feature": 110},
         {"id": 4, "feature": 120},
     ]
-    return spark_session.read.json(spark_context.parallelize(data, 1))
+    return spark_session.read.json(
+        spark_context.parallelize(data).map(lambda x: json.dumps(x))
+    )
 
 
 @fixture
