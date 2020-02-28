5bcbb91c4bff1d9de56d99326cba60a6e4be8074
Mayara Moromisato <may.alveslima@gmail.com>
2020-02-28 16:08:53 -0300

    Change localstack on drone.

--- tests/integration/butterfree/core/load/conftest.py
+++ tests/integration/butterfree/core/load/conftest.py
@@ -63,10 +63,10 @@ def feature_set():
 
 
 def get_s3_client():
-    return boto3.client(
+    return boto3.client(  # noqa: S106
         "s3",
-        aws_access_key_id="",
-        aws_secret_access_key="",
+        aws_access_key_id='',
+        aws_secret_access_key='',
         region_name="us-east-1",
         endpoint_url="http://localhost:4572",
     )
