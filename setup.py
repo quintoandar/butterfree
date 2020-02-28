2a969477676931d53009690976458029c6f5eb00
Rafael Leinio <rafael.leper@gmail.com>
2020-02-28 18:32:53 -0300

    changing setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 __package_name__ = "quintoandar-butterfree"
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 __repository_url__ = "https://github.com/quintoandar/butterfree"
 
 with open("requirements.txt") as f:
