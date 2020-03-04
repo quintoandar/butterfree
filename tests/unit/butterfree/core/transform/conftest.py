6ee115d4dca7e34879080c6a5f6ce62af82533f2
AlvaroMarquesAndrade <alvaro.marques.andrade@gmail.com>
2020-03-04 20:14:41 -0300

    added mocked data

--- tests/unit/butterfree/core/transform/conftest.py
+++ tests/unit/butterfree/core/transform/conftest.py
@@ -1,3 +1,4 @@
+import json
 from unittest.mock import Mock
 
 from pytest import fixture
@@ -44,6 +45,62 @@ def make_dataframe(spark_context, spark_session):
     return df
 
 
+def make_rolling_windows_agg_dataframe(spark_context, spark_session):
+    data = [
+        {
+            "id": 1,
+            "timestamp": "2016-04-11 00:00:00",
+            "feature1__avg_over_1_week_rolling_windows": None,
+        },
+        {
+            "id": 1,
+            "timestamp": "2016-04-12 00:00:00",
+            "feature1__avg_over_1_week_rolling_windows": 350.0,
+        },
+        {
+            "id": 1,
+            "timestamp": "2016-04-13 00:00:00",
+            "feature1__avg_over_1_week_rolling_windows": 350.0,
+        },
+        {
+            "id": 1,
+            "timestamp": "2016-04-14 00:00:00",
+            "feature1__avg_over_1_week_rolling_windows": 350.0,
+        },
+        {
+            "id": 1,
+            "timestamp": "2016-04-15 00:00:00",
+            "feature1__avg_over_1_week_rolling_windows": 350.0,
+        },
+        {
+            "id": 1,
+            "timestamp": "2016-04-16 00:00:00",
+            "feature1__avg_over_1_week_rolling_windows": 350.0,
+        },
+        {
+            "id": 1,
+            "timestamp": "2016-04-17 00:00:00",
+            "feature1__avg_over_1_week_rolling_windows": 350.0,
+        },
+        {
+            "id": 1,
+            "timestamp": "2016-04-18 00:00:00",
+            "feature1__avg_over_1_week_rolling_windows": 350.0,
+        },
+        {
+            "id": 1,
+            "timestamp": "2016-04-19 00:00:00",
+            "feature1__avg_over_1_week_rolling_windows": None,
+        },
+    ]
+    df = spark_session.read.json(
+        spark_context.parallelize(data).map(lambda x: json.dumps(x))
+    )
+    df = df.withColumn("timestamp", df.timestamp.cast(DataType.TIMESTAMP.value))
+
+    return df
+
+
 def make_fs(spark_context, spark_session):
     df = make_dataframe(spark_context, spark_session)
     df = (
@@ -63,6 +120,11 @@ def dataframe(spark_context, spark_session):
 @fixture
 def feature_set_dataframe(spark_context, spark_session):
     return make_fs(spark_context, spark_session)
+
+
+@fixture
+def rolling_windows_agg_dataframe(spark_context, spark_session):
+    return make_rolling_windows_agg_dataframe(spark_context, spark_session)
 
 
 @fixture
