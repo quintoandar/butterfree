6a2cdb86fc14654797bb504712407615e562d91c
Mayara Moromisato <44944954+moromimay@users.noreply.github.com>
2020-03-16 15:00:43 -0300

    [MLOP-181] FileReader update docstring with schema usage (#93)
    * Add method schema in spark.

--- butterfree/core/extract/readers/file_reader.py
+++ butterfree/core/extract/readers/file_reader.py
@@ -1,6 +1,7 @@
 """FileReader entity."""
 
 from pyspark.sql import DataFrame
+from pyspark.sql.types import StructType
 
 from butterfree.core.clients import SparkClient
 from butterfree.core.extract.readers.reader import Reader
@@ -13,6 +14,7 @@ class FileReader(Reader):
         id: unique string id for register the reader as a view on the metastore.
         path: file location.
         format: can be one of the keys: json, parquet, orc, or csv.
+        schema: an optional pyspark.sql.types.StructType for the input schema.
         format_options: additional options required by some formats. Check docs:
             https://spark.apache.org/docs/latest/sql-data-sources-load-save-functions.html#manually-specifying-options
 
@@ -29,6 +31,26 @@ class FileReader(Reader):
     ...               )
     >>> df = file_reader.consume(spark_client)
 
+        However, we can define the schema and format_options,
+        like header, and provide them to FileReader.
+    >>> spark_client = SparkClient()
+    >>> schema_csv = StructType([
+    ...            StructField("column_a", LongType()),
+    ...            StructField("column_b", DoubleType()),
+    ...            StructField("coumn_c", StringType())
+    ...          ])
+    >>> file_reader = FileReader(
+    ...                 id="file_reader_id",
+    ...                 path="data_path",
+    ...                 format="csv",
+    ...                 schema=schema_csv,
+    ...                 format_options={
+    ...                    "header": True
+    ...                 }
+    ...               )
+    >>> df = file_reader.consume(spark_client)
+
+
         This last method will use the Spark Client, as default, to read
         the desired file, loading data into a dataframe, according to
         FileReader class arguments.
@@ -42,7 +64,14 @@ class FileReader(Reader):
 
     """
 
-    def __init__(self, id: str, path: str, format: str, format_options: dict = None):
+    def __init__(
+        self,
+        id: str,
+        path: str,
+        format: str,
+        schema: StructType = None,
+        format_options: dict = None,
+    ):
         super().__init__(id)
         if not isinstance(path, str):
             raise ValueError("path must be a string with the file location")
@@ -50,6 +79,7 @@ class FileReader(Reader):
             raise ValueError("format must be a string with the file type")
         self.path = path
         self.format = format
+        self.schema = schema
         self.options = dict(
             {"path": self.path}, **format_options if format_options else {}
         )
@@ -64,4 +94,4 @@ class FileReader(Reader):
             Dataframe with all the files data.
 
         """
-        return client.read(self.format, self.options)
+        return client.read(self.format, self.options, self.schema)
