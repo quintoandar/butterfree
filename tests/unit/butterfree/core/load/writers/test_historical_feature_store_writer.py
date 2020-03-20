dd2c8a2780dbf66657a55d2364c47170d3b10e81
Rafael Leinio <rafael.leper@gmail.com>
2020-03-20 17:10:36 -0300

    flake8

--- tests/unit/butterfree/core/load/writers/test_historical_feature_store_writer.py
+++ tests/unit/butterfree/core/load/writers/test_historical_feature_store_writer.py
@@ -82,10 +82,17 @@ class TestHistoricalFeatureStoreWriter:
         # arrange
         start = datetime.datetime(year=1970, month=1, day=1)
         end = datetime.datetime(year=2020, month=12, day=31)
-        random_ts = lambda: start + datetime.timedelta(
-            seconds=random.randint(0, int((end - start).total_seconds()))
-        )
-        random_dates = [random_ts().date().isoformat() for _ in range(10000)]
+        random_dates = [
+            (
+                lambda: start
+                + datetime.timedelta(
+                    seconds=random.randint(0, int((end - start).total_seconds()))
+                )  # noqa: S311
+            )()
+            .date()
+            .isoformat()
+            for _ in range(10000)
+        ]
         data = [{"timestamp": date} for date in random_dates]
         input_df = spark_session.read.json(
             spark_context.parallelize(data, 1), schema="timestamp timestamp"
