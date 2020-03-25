7c4f8817e3fd2e3e111880fc3389f695b9ffe436
AlvaroMarquesAndrade <45604858+AlvaroMarquesAndrade@users.noreply.github.com>
2020-03-25 16:35:08 -0300

    [MLOP-176] MostFrequent aggregation (#104)

---
+++ butterfree/core/transform/transformations/user_defined_functions/mode.py
@@ -0,0 +1,58 @@
+"""Method to compute mode aggregation."""
+from pyspark.sql.functions import PandasUDFType, pandas_udf
+from pyspark.sql.types import StringType
+
+
+@pandas_udf(StringType(), PandasUDFType.GROUPED_AGG)
+def mode(column):
+    """Computes a mode aggregation.
+
+    Attributes:
+        column: desired data to be aggregated with mode.
+
+    Example:
+        It's necessary to declare the desired aggregation method, (average,
+        standard deviation and count are currently supported, as it can be
+        seen in __ALLOWED_AGGREGATIONS), the partition column, choose both
+        window lenght and time unit and, finally, define the mode.
+        >>> from pyspark import SparkContext
+        >>> from pyspark.sql import session, Window
+        >>> from pyspark.sql.functions import PandasUDFType, pandas_udf
+        >>> from pyspark.sql.types import StringType
+        >>> sc = SparkContext.getOrCreate()
+        >>> spark = session.SparkSession(sc)
+        >>> df = spark.createDataFrame(
+        >>>      [(1, 1), (1, 1), (2, 2), (2, 1), (2, 2)],
+        >>>      ("id", "column"))
+        >>> @pandas_udf(StringType(), PandasUDFType.GROUPED_AGG)
+        ... def mode(column):
+        ...    return str(column.mode()[0])
+        >>> df.groupby("id").agg(mode("column")).show()
+        +---+------------+
+        | id|mode(column)|
+        +---+------------+
+        |  1|           1|
+        |  2|           2|
+        +---+------------+
+        >>> w = Window \
+        ...     .partitionBy('id') \
+        ...     .rowsBetween(Window.unboundedPreceding, Window.unboundedFollowing)
+        >>> df.withColumn('most_viewed', mode("column").over(w)).show()
+        +---+------+-----------+
+        | id|column|most_viewed|
+        +---+------+-----------+
+        |  1|     1|          1|
+        |  1|     1|          1|
+        |  2|     2|          2|
+        |  2|     1|          2|
+        |  2|     2|          2|
+        +---+------+-----------+
+
+        This example shows the mode aggregation. It's important to notice,
+        however, that if we want to used in fixed_windows or row_windows mode,
+        we'd need unbounded windows. For that reason, mode is meant to be used
+        just in rolling_windows mode, initially. We intend to make it available
+        to others modes soon.
+
+    """
+    return str(column.mode()[0])
