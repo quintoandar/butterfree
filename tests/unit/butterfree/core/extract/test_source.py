3dc2ada1dacfa3a2b37f618ed4ced4625380804b
AlvaroMarquesAndrade <alvaro.marques.andrade@gmail.com>
2020-03-26 16:40:33 -0300

    add test

--- tests/unit/butterfree/core/extract/test_source.py
+++ tests/unit/butterfree/core/extract/test_source.py
@@ -38,3 +38,25 @@ class TestSource:
         result_df = source_selector.construct(spark_client)
 
         assert result_df.is_cached
+
+    def test_is_repartitioned(self, mocker, target_df):
+        # given
+        spark_client = SparkClient()
+
+        reader_id = "a_source"
+        reader = mocker.stub(reader_id)
+        reader.build = mocker.stub("build")
+        reader.build.side_effect = target_df.createOrReplaceTempView(reader_id)
+
+        # when
+        source_selector = Source(
+            readers=[reader],
+            query=f"select * from {reader_id}",  # noqa
+            partition_by=["col1"],
+            num_partitions=2,
+        )
+
+        result_df = source_selector.construct(spark_client)
+
+        assert result_df.is_cached
+        assert result_df.collect() == target_df.collect()
