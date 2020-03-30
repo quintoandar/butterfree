6deb0f84a03a22e3029f023273fd4ac3bc5cbd13
Mayara Moromisato <may.alveslima@gmail.com>
2020-03-30 19:14:25 -0300

    Fix test pipeline.

--- butterfree/core/transform/aggregated_feature_set.py
+++ butterfree/core/transform/aggregated_feature_set.py
@@ -53,6 +53,7 @@ class AggregatedFeatureSet(FeatureSet):
     ...    keys=[KeyFeature(name="id", description="The user's Main ID or device ID")],
     ...    timestamp=TimestampFeature(),
     ...)
+
     >>> feature_set.construct(dataframe=dataframe)
 
     The construct method will execute the feature set, computing all the
