a942b0318c51a161e95aa0aaa56d07c3eca669e3
AlvaroMarquesAndrade <alvaro.marques.andrade@gmail.com>
2020-03-30 19:23:19 -0300

    added them to __init__

--- butterfree/core/clients/__init__.py
+++ butterfree/core/clients/__init__.py
@@ -1,5 +1,6 @@
 """Holds connection clients."""
+from butterfree.core.clients.abstract_client import AbstractClient
 from butterfree.core.clients.cassandra_client import CassandraClient
 from butterfree.core.clients.spark_client import SparkClient
 
-__all__ = ["SparkClient", "CassandraClient"]
+__all__ = ["SparkClient", "CassandraClient", "AbstractClient"]
