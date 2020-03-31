583f6d337c67433c27412530eb5e99692ec3b352
AlvaroMarquesAndrade <alvaro.marques.andrade@gmail.com>
2020-03-31 12:54:40 -0300

    added tests to source

--- tests/unit/butterfree/core/extract/test_source.py
+++ tests/unit/butterfree/core/extract/test_source.py
@@ -1,3 +1,5 @@
+from pyspark.sql.functions import spark_partition_id
+
 from butterfree.core.clients import SparkClient
 from butterfree.core.extract import Source
 
@@ -39,24 +41,32 @@ class TestSource:
 
         assert result_df.is_cached
 
-    def test_is_repartitioned(self, mocker, target_df):
+    def test_construct_with_repartition(self, mocker, feature_set_dataframe):
         # given
         spark_client = SparkClient()
 
         reader_id = "a_source"
         reader = mocker.stub(reader_id)
         reader.build = mocker.stub("build")
-        reader.build.side_effect = target_df.createOrReplaceTempView(reader_id)
+        reader.build.side_effect = feature_set_dataframe.createOrReplaceTempView(
+            reader_id
+        )
 
         # when
         source_selector = Source(
-            readers=[reader],
-            query=f"select * from {reader_id}",  # noqa
-            partition_by=["col1"],
-            num_partitions=2,
+            readers=[reader], query=f"select * from {reader_id}",  # noqa
         )
 
-        result_df = source_selector.construct(spark_client)
+        result_df = source_selector.construct(
+            spark_client, partition_by=["id"], num_partitions=2
+        )
 
+        # assert
         assert result_df.is_cached
-        assert result_df.collect() == target_df.collect()
+        assert sorted(result_df.collect()) == sorted(feature_set_dataframe.collect())
+        # Only one partition id, meaning data is not partitioned
+        assert (
+            feature_set_dataframe.select(spark_partition_id()).distinct().count() == 1
+        )
+        # Desired number of partitions
+        assert result_df.select(spark_partition_id()).distinct().count() == 2
