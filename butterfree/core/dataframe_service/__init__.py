fe4c32b1dcb82279ea8f2540f0b9a6bd62f3fd68
AlvaroMarquesAndrade <alvaro.marques.andrade@gmail.com>
2020-03-31 12:50:57 -0300

    updated package name

---
+++ butterfree/core/dataframe_service/__init__.py
@@ -0,0 +1,7 @@
+"""Dataframe optimization components regarding Butterfree."""
+from butterfree.core.dataframe_service.repartition import (
+    repartition_df,
+    repartition_sort_df,
+)
+
+__all__ = ["repartition_df", "repartition_sort_df"]
--- butterfree/core/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Util Components regarding Butterfree."""
-from butterfree.core.utils.repartition import repartition_df, repartition_sort_df
-
-__all__ = ["repartition_df", "repartition_sort_df"]
