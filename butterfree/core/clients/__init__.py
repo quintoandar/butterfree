7b371115470f163b992dee80073659385415edca
AlvaroMarquesAndrade <45604858+AlvaroMarquesAndrade@users.noreply.github.com>
2020-03-31 18:20:37 -0300

    [MLOP-141] Create client cassandra on butterfree (#109)

--- butterfree/core/clients/__init__.py
+++ butterfree/core/clients/__init__.py
@@ -1,4 +1,6 @@
 """Holds connection clients."""
+from butterfree.core.clients.abstract_client import AbstractClient
+from butterfree.core.clients.cassandra_client import CassandraClient
 from butterfree.core.clients.spark_client import SparkClient
 
-__all__ = ["SparkClient"]
+__all__ = ["SparkClient", "CassandraClient", "AbstractClient"]
