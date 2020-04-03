4956408e53ea64eca76c562b0d4daa90da876b5c
AlvaroMarquesAndrade <45604858+AlvaroMarquesAndrade@users.noreply.github.com>
2020-04-03 16:34:50 -0300

    [MLOP-228] Remove cross join from AggregatedFeatureSet (#123)

--- butterfree/core/transform/aggregated_feature_set.py
+++ butterfree/core/transform/aggregated_feature_set.py
@@ -1,13 +1,10 @@
 """AggregatedFeatureSet entity."""
-from datetime import datetime
 from functools import reduce
 from typing import List
 
 from pyspark.sql import DataFrame, functions
 
 from butterfree.core.clients import SparkClient
-from butterfree.core.constants.columns import TIMESTAMP_COLUMN
-from butterfree.core.constants.data_type import DataType
 from butterfree.core.transform import FeatureSet
 from butterfree.core.transform.features import Feature
 from butterfree.core.transform.transformations import AggregatedTransform
@@ -139,102 +136,6 @@ class AggregatedFeatureSet(FeatureSet):
         """
         return all([not feature.transformation.has_windows for feature in features])
 
-    @staticmethod
-    def _generate_date_sequence_df(client: SparkClient, date_range, step=None):
-        """Generates a date sequence dataframe from a given date range.
-
-        Create a Spark DataFrame with a single column named timestamp and a
-        range of dates within the desired interval (start and end dates included).
-        It's also possible to provide a step argument.
-
-        Attributes:
-            client:  spark client used to create the dataframe.
-            date_range: list of the desired date interval.
-            step: time step.
-        """
-        day_in_seconds = 60 * 60 * 24
-        start_date, end_date = date_range
-        step = step or day_in_seconds
-        date_df = client.conn.createDataFrame(
-            [(start_date, end_date)], ("start_date", "end_date")
-        ).select(
-            [
-                functions.col(c)
-                .cast(DataType.TIMESTAMP.spark)
-                .cast(DataType.BIGINT.spark)
-                for c in ("start_date", "end_date")
-            ]
-        )
-        start_date, end_date = date_df.first()
-        return client.conn.range(start_date, end_date + day_in_seconds, step).select(
-            functions.col("id").cast(DataType.TIMESTAMP.spark).alias(TIMESTAMP_COLUMN)
-        )
-
-    def _get_unique_keys(self, output_df):
-        """Get key columns unique values.
-
-        Create a Spark DataFrame with unique key columns values.
-
-        Attributes:
-            output_df:  dataframe to get unique key values.
-        """
-        return output_df.dropDuplicates(subset=self.keys_columns).select(
-            self.keys_columns
-        )
-
-    @staticmethod
-    def _set_cross_join_confs(client, state):
-        """Defines spark configuration."""
-        client.conn.conf.set("spark.sql.crossJoin.enabled", state)
-        client.conn.conf.set("spark.sql.autoBroadcastJoinThreshold", int(state))
-
-    def _cross_join_df(self, client: SparkClient, first_df, second_df):
-        """Cross join between desired dataframes.
-
-        Returns a cross join between the date daframe and transformed
-        dataframe. In order to make this operation faster, the smaller
-        dataframe is cached and autoBroadcastJoinThreshold conf is
-        setted to False.
-
-        Attributes:
-            client:  spark client used to create the dataframe.
-            first_df: a generic dataframe.
-            second_df: another generic dataframe.
-        """
-        self._set_cross_join_confs(client, True)
-        first_df.cache().take(
-            1
-        ) if second_df.count() >= first_df.count() else second_df.cache().take(1)
-        cross_join_df = first_df.join(second_df)
-        cross_join_df.cache().take(1)
-        self._set_cross_join_confs(client, False)
-        return cross_join_df
-
-    def _create_date_range_dataframe(self, client: SparkClient, dataframe, end_date):
-        """Performs a join between dataframes.
-
-        Performs a join between the transformed dataframe and the date
-        dataframe.
-
-        Attributes:
-            dataframe:  source dataframe.
-            output_df: transformed dataframe.
-            client: client responsible for connecting to Spark session.
-        """
-        start_date = dataframe.select(functions.min(TIMESTAMP_COLUMN)).collect()[0][0]
-        end_date = end_date or datetime.now()
-        date_range = [
-            start_date
-            if isinstance(start_date, str)
-            else start_date.strftime("%Y-%m-%d"),
-            end_date if isinstance(end_date, str) else end_date.strftime("%Y-%m-%d"),
-        ]
-        date_df = self._generate_date_sequence_df(client, date_range)
-        unique_df = self._get_unique_keys(dataframe)
-        cross_join_df = self._cross_join_df(client, date_df, unique_df)
-
-        return cross_join_df
-
     def _dataframe_join(self, left, right, on, how):
         return left.join(right, on=on, how=how)
 
@@ -271,17 +172,19 @@ class AggregatedFeatureSet(FeatureSet):
             df_list.append(feature_df)
 
         if self._has_aggregated_transform_with_window_only(self.features):
-            range_df = self._create_date_range_dataframe(client, output_df, end_date)
+
             output_df = reduce(
                 lambda left, right: self._dataframe_join(
                     left,
                     right,
                     on=self.keys_columns + [self.timestamp_column],
-                    how="left",
+                    how="full_outer",
                 ),
                 df_list,
-                range_df,
             )
+
+            if end_date:
+                output_df = output_df.filter(f"{self.timestamp_column} <= '{end_date}'")
 
         elif self._has_aggregated_transform_without_window_only(self.features):
             agg_df = output_df.groupBy(self.keys_columns).agg(
