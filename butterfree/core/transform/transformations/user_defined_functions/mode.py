386178c2376de37524bcebca1d1084de3fcd882d
AlvaroMarquesAndrade <45604858+AlvaroMarquesAndrade@users.noreply.github.com>
2020-04-30 14:36:17 -0300

    [MLOP-279] Refactor Mode (#143)

--- butterfree/core/transform/transformations/user_defined_functions/mode.py
+++ butterfree/core/transform/transformations/user_defined_functions/mode.py
@@ -26,13 +26,13 @@ def mode(column):
         >>>      ("id", "column"))
         >>> @pandas_udf(StringType(), PandasUDFType.GROUPED_AGG)
         ... def mode(column):
-        ...    return str(column.mode()[0])
+        ...    return str(column.value_counts().index.tolist()[:10])
         >>> df.groupby("id").agg(mode("column")).show()
         +---+------------+
         | id|mode(column)|
         +---+------------+
-        |  1|           1|
-        |  2|           2|
+        |  1|         [1]|
+        |  2|      [2, 1]|
         +---+------------+
         >>> w = Window \
         ...     .partitionBy('id') \
@@ -41,18 +41,18 @@ def mode(column):
         +---+------+-----------+
         | id|column|most_viewed|
         +---+------+-----------+
-        |  1|     1|          1|
-        |  1|     1|          1|
-        |  2|     2|          2|
-        |  2|     1|          2|
-        |  2|     2|          2|
+        |  1|     1|        [1]|
+        |  1|     1|        [1]|
+        |  2|     2|     [2, 1]|
+        |  2|     1|     [2, 1]|
+        |  2|     2|     [2, 1]|
         +---+------+-----------+
 
-        This example shows the mode aggregation. It's important to notice,
-        however, that if we want to used in fixed_windows or row_windows mode,
-        we'd need unbounded windows. For that reason, mode is meant to be used
-        just in rolling_windows mode, initially. We intend to make it available
-        to others modes soon.
+        This example shows the mode aggregation. It returns a list with the most
+        frequent values (limited to ten). It's important to notice, however, that
+        if we want to used in fixed_windows or row_windows mode, we'd need unbounded
+        windows. For that reason, mode is meant to be used just in rolling_windows
+        mode, initially. We intend to make it available to others modes soon.
 
     """
-    return str(column.mode()[0])
+    return str(column.value_counts().index.tolist()[:10])
