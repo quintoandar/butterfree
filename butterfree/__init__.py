6aca639437c500a34354e94eb02a4e941c95df5c
Mayara Moromisato <44944954+moromimay@users.noreply.github.com>
2020-07-08 17:39:44 -0300

    [MLOP-384] Make imports more simple (#184)
    * Testing imports.
    
    * Simple importing.
    
    * Fix lint.

--- butterfree/__init__.py
+++ butterfree/__init__.py
@@ -1,1 +1,86 @@
 """Module docstring example, following Google's docstring style."""
+from butterfree.core.clients import AbstractClient, CassandraClient, SparkClient
+from butterfree.core.configs.db import AbstractWriteConfig, CassandraConfig, S3Config
+from butterfree.core.constants.data_type import DataType
+from butterfree.core.dataframe_service import repartition_df, repartition_sort_df
+from butterfree.core.extract import Source
+from butterfree.core.extract.pre_processing import (
+    explode_json_column,
+    filter,
+    forward_fill,
+    pivot,
+    replace,
+)
+from butterfree.core.extract.readers import FileReader, KafkaReader, TableReader
+from butterfree.core.load import Sink
+from butterfree.core.load.writers import (
+    HistoricalFeatureStoreWriter,
+    OnlineFeatureStoreWriter,
+)
+from butterfree.core.pipelines.feature_set_pipeline import FeatureSetPipeline
+from butterfree.core.transform import FeatureSet
+from butterfree.core.transform.aggregated_feature_set import AggregatedFeatureSet
+from butterfree.core.transform.features import Feature, KeyFeature, TimestampFeature
+from butterfree.core.transform.transformations import (
+    AggregatedTransform,
+    CustomTransform,
+    H3HashTransform,
+    SparkFunctionTransform,
+    SQLExpressionTransform,
+    StackTransform,
+)
+from butterfree.core.transform.transformations.user_defined_functions import (
+    mode,
+    most_frequent_set,
+)
+from butterfree.core.transform.utils import Function, Window
+from butterfree.core.validations import ValidateDataframe
+from butterfree.testing.dataframe import (
+    assert_column_equality,
+    assert_dataframe_equality,
+    create_df_from_collection,
+)
+
+__all__ = [
+    "AbstractClient",
+    "CassandraClient",
+    "SparkClient",
+    "AbstractWriteConfig",
+    "CassandraConfig",
+    "S3Config",
+    "DataType",
+    "repartition_df",
+    "repartition_sort_df",
+    "Source",
+    "explode_json_column",
+    "filter",
+    "forward_fill",
+    "pivot",
+    "replace",
+    "FileReader",
+    "KafkaReader",
+    "TableReader",
+    "Sink",
+    "HistoricalFeatureStoreWriter",
+    "OnlineFeatureStoreWriter",
+    "FeatureSetPipeline",
+    "AggregatedFeatureSet",
+    "FeatureSet",
+    "Feature",
+    "KeyFeature",
+    "TimestampFeature",
+    "AggregatedTransform",
+    "CustomTransform",
+    "H3HashTransform",
+    "SparkFunctionTransform",
+    "SQLExpressionTransform",
+    "StackTransform",
+    "mode",
+    "most_frequent_set",
+    "Function",
+    "Window",
+    "ValidateDataframe",
+    "assert_column_equality",
+    "assert_dataframe_equality",
+    "create_df_from_collection",
+]
--- butterfree/core/transform/utils/__init__.py
+++ butterfree/core/transform/utils/__init__.py
@@ -1,4 +1,5 @@
 """This module holds utils to be used by transformations."""
+from butterfree.core.transform.utils.function import Function
 from butterfree.core.transform.utils.window_spec import Window
 
-__all__ = ["Window"]
+__all__ = ["Window", "Function"]
