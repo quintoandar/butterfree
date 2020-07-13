1d8c943b5f83bade08ec30b145d161d3055c7494
Rafael Leiniö <34458243+rafaelleinio@users.noreply.github.com>
2020-07-13 18:10:10 -0300

    [MLOP-375] Refactor Sink and OnlineFeatureStore write to entity flow (#182)

--- tests/unit/butterfree/core/validations/test_basic_validation.py
+++ tests/unit/butterfree/core/validations/test_basic_validation.py
@@ -1,24 +1,24 @@
 import pytest
 
-from butterfree.core.validations import ValidateDataframe
+from butterfree.core.validations import BasicValidation
 
 
-class TestValidateDataframe:
+class TestBasicValidation:
     def test_validate_without_column_ts(self, feature_set_without_ts):
-        check = ValidateDataframe(feature_set_without_ts)
+        check = BasicValidation(feature_set_without_ts)
 
         with pytest.raises(ValueError):
             check.validate_column_ts()
 
     def test_validate_empty(self, feature_set_empty):
-        check = ValidateDataframe(feature_set_empty)
+        check = BasicValidation(feature_set_empty)
 
         with pytest.raises(ValueError):
             check.validate_df_is_empty()
 
     def test_validate_not_spark_df(self):
         df_writer = "not a spark df writer"
-        check = ValidateDataframe(df_writer)
+        check = BasicValidation(df_writer)
 
         with pytest.raises(ValueError):
             check.validate_df_is_spark_df()
