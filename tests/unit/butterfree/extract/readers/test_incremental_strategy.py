a3fd8814a579919ebbe4eba1dc76bbdf1e661a87
Rafael Leinio <rafael.leper@gmail.com>
2020-08-04 00:41:21 -0300

    readers incremental strategy

---
+++ tests/unit/butterfree/extract/readers/test_incremental_strategy.py
@@ -0,0 +1,68 @@
+from butterfree.extract.readers import IncrementalStrategy
+
+
+class TestIncrementalStrategy:
+    def test_from_milliseconds(self):
+        # arrange
+        incremental_strategy = IncrementalStrategy().from_milliseconds("ts")
+        target_expression = "date(from_unixtime(ts/ 1000.0)) >= date('2020-01-01')"
+
+        # act
+        result_expression = incremental_strategy.get_expression(start_date="2020-01-01")
+
+        # assert
+        assert target_expression.split() == result_expression.split()
+
+    def test_from_string(self):
+        # arrange
+        incremental_strategy = IncrementalStrategy().from_string(
+            "dt", mask="dd/MM/yyyy"
+        )
+        target_expression = "to_date(dt, 'dd/MM/yyyy') >= date('2020-01-01')"
+
+        # act
+        result_expression = incremental_strategy.get_expression(start_date="2020-01-01")
+
+        # assert
+        assert target_expression.split() == result_expression.split()
+
+    def test_from_year_month_day_partitions(self):
+        # arrange
+        incremental_strategy = IncrementalStrategy().from_year_month_day_partitions(
+            year_column="y", month_column="m", day_column="d"
+        )
+        target_expression = (
+            "date(concat(string(y), "
+            "'-', string(m), "
+            "'-', string(d))) >= date('2020-01-01')"
+        )
+
+        # act
+        result_expression = incremental_strategy.get_expression(start_date="2020-01-01")
+
+        # assert
+        assert target_expression.split() == result_expression.split()
+
+    def test_get_expression_with_just_end_date(self):
+        # arrange
+        incremental_strategy = IncrementalStrategy(column="dt")
+        target_expression = "dt <= date('2020-01-01')"
+
+        # act
+        result_expression = incremental_strategy.get_expression(end_date="2020-01-01")
+
+        # assert
+        assert target_expression.split() == result_expression.split()
+
+    def test_get_expression_with_start_and_end_date(self):
+        # arrange
+        incremental_strategy = IncrementalStrategy(column="dt")
+        target_expression = "dt >= date('2019-12-30') and dt <= date('2020-01-01')"
+
+        # act
+        result_expression = incremental_strategy.get_expression(
+            start_date="2019-12-30", end_date="2020-01-01"
+        )
+
+        # assert
+        assert target_expression.split() == result_expression.split()
