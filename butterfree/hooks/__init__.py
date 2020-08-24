25b325f560df3351db5387498f6a7b97b7a7dfbe
Rafael Leiniö <34458243+rafaelleinio@users.noreply.github.com>
2020-08-24 15:28:55 -0300

    [MLOP-455] Spark Table Schema Compatibility Hook (#233)

---
+++ butterfree/hooks/__init__.py
@@ -0,0 +1,1 @@
+"""Holds Hooks definitions."""
---
+++ butterfree/hooks/schema_compatibility/__init__.py
@@ -0,0 +1,5 @@
+from butterfree.hooks.schema_compatibility.spark_table_schema_compatibility_hook import (  # noqa
+    SparkTableSchemaCompatibilityHook,
+)
+
+__all__ = ["SparkTableSchemaCompatibilityHook"]
