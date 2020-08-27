8aa22c12217ca7cd7acd179ede6e6aa63fef86e5
Mayara Moromisato <may.alveslima@gmail.com>
2020-08-27 15:15:50 -0300

    Create cassandra check schema hook.

--- butterfree/hooks/schema_compatibility/__init__.py
+++ butterfree/hooks/schema_compatibility/__init__.py
@@ -1,5 +1,8 @@
+from butterfree.hooks.schema_compatibility.cassandra_table_schema_compatibility_hook import (
+    CassandraTableSchemaCompatibilityHook,
+)
 from butterfree.hooks.schema_compatibility.spark_table_schema_compatibility_hook import (  # noqa
     SparkTableSchemaCompatibilityHook,
 )
 
-__all__ = ["SparkTableSchemaCompatibilityHook"]
+__all__ = ["SparkTableSchemaCompatibilityHook", "CassandraTableSchemaCompatibilityHook"]
