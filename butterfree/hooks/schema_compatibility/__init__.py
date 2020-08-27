2bef317efb779a89db797e3a4c49144fec3c5f83
Mayara Moromisato <may.alveslima@gmail.com>
2020-08-27 16:03:47 -0300

    Apply quality style

--- butterfree/hooks/schema_compatibility/__init__.py
+++ butterfree/hooks/schema_compatibility/__init__.py
@@ -1,4 +1,5 @@
-from butterfree.hooks.schema_compatibility.cassandra_table_schema_compatibility_hook import (
+"""Holds Schema Compatibility Hooks definitions."""
+from butterfree.hooks.schema_compatibility.cassandra_table_schema_compatibility_hook import (  # noqa
     CassandraTableSchemaCompatibilityHook,
 )
 from butterfree.hooks.schema_compatibility.spark_table_schema_compatibility_hook import (  # noqa
