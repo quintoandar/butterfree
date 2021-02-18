a59030dc873c4c3731594b28231e7490adaec673
AlvaroMarquesAndrade <1a789766b1c4c8b679e80f11fa6d63d42fa4bcdf>
2021-02-18 11:06:11 -0300

    fix classes

--- butterfree/migrations/cassandra_migration.py
+++ butterfree/migrations/cassandra_migration.py
@@ -1,14 +1,19 @@
 """Cassandra Migration entity."""
 
-from typing import Any
+from typing import Any, Dict, List
 
-from butterfree.migrations import Migration
+from butterfree.migrations import DatabaseMigration
 
 
-class CassandraMigration(Migration):
+class CassandraMigration(DatabaseMigration):
     """Cassandra class for Migrations."""
 
-    def create_query(self) -> Any:
+    def create_query(
+        self,
+        fs_schema: List[Dict[str, Any]],
+        db_schema: List[Dict[str, Any]],
+        table_name: str,
+    ) -> Any:
         """Create a query regarding Cassandra.
 
         Returns:
--- butterfree/migrations/metastore_migration.py
+++ butterfree/migrations/metastore_migration.py
@@ -1,14 +1,19 @@
 """Metastore Migration entity."""
 
-from typing import Any
+from typing import Any, Dict, List
 
-from butterfree.migrations import Migration
+from butterfree.migrations import DatabaseMigration
 
 
-class MetastoreMigration(Migration):
+class MetastoreMigration(DatabaseMigration):
     """Metastore class for Migrations."""
 
-    def create_query(self) -> Any:
+    def create_query(
+        self,
+        fs_schema: List[Dict[str, Any]],
+        db_schema: List[Dict[str, Any]],
+        table_name: str,
+    ) -> Any:
         """Create a query regarding Metastore.
 
         Returns:
