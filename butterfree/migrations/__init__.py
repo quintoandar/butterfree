9074c3c8c4467f3260de442ec081b6c4b6e55cb0
AlvaroMarquesAndrade <1a789766b1c4c8b679e80f11fa6d63d42fa4bcdf>
2021-02-18 10:57:38 -0300

    fix init

--- butterfree/migrations/__init__.py
+++ butterfree/migrations/__init__.py
@@ -2,6 +2,6 @@
 
 from butterfree.migrations.cassandra_migration import CassandraMigration
 from butterfree.migrations.metastore_migration import MetastoreMigration
-from butterfree.migrations.migration import Migration
+from butterfree.migrations.migration import DatabaseMigration
 
-__all__ = ["Migration", "CassandraMigration", "MetastoreMigration"]
+__all__ = ["DatabaseMigration", "CassandraMigration", "MetastoreMigration"]
