94e497d2bc4cb221d30e131ec027e60770be1997
AlvaroMarquesAndrade <1a789766b1c4c8b679e80f11fa6d63d42fa4bcdf>
2021-03-11 18:41:15 -0300

    apply suggested changes

--- butterfree/migrations/database_migration/metastore_migration.py
+++ butterfree/migrations/database_migration/metastore_migration.py
@@ -1,15 +1,33 @@
 """Metastore Migration entity."""
 
-import warnings
-from typing import Any, Dict, List
+import logging
+from typing import Any, Dict, List, Set
 
 from butterfree.configs import environment
 from butterfree.constants.migrations import PARTITION_BY
-from butterfree.migrations.database_migration.database_migration import DatabaseMigration
+from butterfree.migrations.database_migration.database_migration import (
+    DatabaseMigration,
+    Diff,
+)
 
 
 class MetastoreMigration(DatabaseMigration):
-    """Metastore class for Migrations."""
+    """MetastoreMigration class for performing migrations.
+
+    This class implements some methods of the parent DatabaseMigration class and
+    has specific methods for query building.
+    The MetastoreMigration class will be used, as the name suggests, for applying
+    changes to a given Metastore table. There are, however, some remarks that need
+    to be highlighted:
+        - If an existing feature has its type changed, then it's extremely important to
+        make sure that this conversion would not result in data loss;
+        - If new features are added to your feature set, then they're going to be added
+        to the corresponding Metastore table;
+        - Since feature sets can be written both to a feature set and an entity table,
+        we're not going to automatically drop features when using entity tables, since
+        it means that some features belong to a different feature set. In summary, if
+        data is being loaded into an entity table, then users can drop columns manually.
+    """
 
     def __init__(
         self, database: str = None,
@@ -19,37 +37,152 @@ class MetastoreMigration(DatabaseMigration):
         )
 
     @staticmethod
-    def _get_parsed_columns(table_columns: List[Dict[str, Any]]) -> List[str]:
+    def _get_parsed_columns(columns: List[Diff]) -> List[str]:
+        """Parse columns from a list of Diff objects.
+
+        Args:
+            columns: list of Diff objects.
+
+        Returns:
+            Parsed columns.
+
+        """
         parsed_columns = []
-        for col in table_columns:
-            parsed_columns.append(f"{col['column_name']} {col['type']}")
+        for col in columns:
+            parsed_columns.append(f"{col.column} {col.value}")
 
         parsed_columns = ", ".join(parsed_columns)  # type: ignore
 
         return parsed_columns
 
-    def _get_alter_table_query(
-        self, table_columns: List[Dict[str, Any]], table_name: str
-    ) -> str:
-        parsed_columns = self._get_parsed_columns(table_columns)
+    def _get_alter_table_add_query(self, columns: List[Diff], table_name: str) -> str:
+        """Creates SQL statement to add columns to a table.
+
+        Args:
+            columns: list of Diff objects with ADD kind.
+            table_name: table name.
+
+        Returns:
+            Alter table query.
+
+        """
+        parsed_columns = self._get_parsed_columns(columns)
 
         return (
-            f"ALTER TABLE {self.database}.{table_name} ADD columns ({parsed_columns});"
+            f"ALTER TABLE {self.database}.{table_name} "
+            f"ADD IF NOT EXISTS columns ({parsed_columns});"
         )
 
+    def _get_alter_column_type_query(self, columns: List[Diff], table_name: str) -> str:
+        """Creates SQL statement to alter columns' types.
+
+        Args:
+            columns: list of Diff objects with ALTER_TYPE kind.
+            table_name: table name.
+
+        Returns:
+            Alter column type query.
+
+        """
+        parsed_columns = self._get_parsed_columns(columns)
+
+        return f"ALTER TABLE {table_name} ALTER COLUMN ({parsed_columns});"
+
     def _get_create_table_query(
-        self, table_columns: List[Dict[str, Any]], table_name: str
+        self, columns: List[Dict[str, Any]], table_name: str
     ) -> str:
-        """Creates SQL statement to create a table."""
-        table_columns.extend(PARTITION_BY)
-        parsed_columns = self._get_parsed_columns(table_columns)
+        """Creates SQL statement to create a table.
+
+        Args:
+            columns: object that contains column's schemas.
+            table_name: table name.
+
+        Returns:
+            Create table query.
+
+        """
+        columns.extend(PARTITION_BY)
+
+        parsed_columns = []
+        for col in columns:
+            parsed_columns.append(f"{col['column_name']} {col['type']}")
+        parsed_columns = ", ".join(parsed_columns)  # type: ignore
 
         return (
-            f"CREATE TABLE {self.database}.{table_name} ({parsed_columns}) "
+            f"CREATE TABLE IF NOT EXISTS  "
+            f"{self.database}.{table_name} ({parsed_columns}) "
             f"PARTITIONED BY ({PARTITION_BY[0]['column_name']}, "
             f"{PARTITION_BY[1]['column_name']}, "
             f"{PARTITION_BY[2]['column_name']});"
         )
+
+    def _get_alter_table_drop_query(self, columns: List[Diff], table_name: str) -> str:
+        """Creates SQL statement to drop columns from a table.
+
+        Args:
+            columns: list of Diff objects with DROP kind.
+            table_name: table name.
+
+        Returns:
+            Drop columns from a given table query.
+
+        """
+        parsed_columns = self._get_parsed_columns(columns)
+
+        return f"ALTER TABLE {table_name} DROP IF EXISTS ({parsed_columns});"
+
+    def _get_queries(
+        self, schema_diff: Set[Diff], table_name: str, write_on_entity: bool = None
+    ) -> List[str]:
+        """Create the desired queries for migration.
+
+        Args:
+            schema_diff: list of Diff objects.
+            table_name: table name.
+
+        Returns:
+            List of queries.
+        """
+        add_items = []
+        drop_items = []
+        alter_type_items = []
+        alter_key_items = []
+
+        for diff in schema_diff:
+            if diff.kind == Diff.Kind.ADD:
+                add_items.append(diff)
+            elif diff.kind == Diff.Kind.ALTER_TYPE:
+                alter_type_items.append(diff)
+            elif diff.kind == Diff.Kind.DROP:
+                drop_items.append(diff)
+            elif diff.kind == Diff.Kind.ALTER_KEY:
+                alter_key_items.append(diff)
+
+        queries = []
+        if add_items:
+            alter_table_add_query = self._get_alter_table_add_query(
+                add_items, table_name
+            )
+            queries.append(alter_table_add_query)
+        if drop_items:
+            if write_on_entity:
+                logging.info(
+                    "Features will not be dropped automatically "
+                    "when data is loaded to an entity table"
+                )
+            drop_columns_query = self._get_alter_table_drop_query(
+                drop_items, table_name
+            )
+            queries.append(drop_columns_query)
+        if alter_type_items:
+            alter_column_types_query = self._get_alter_column_type_query(
+                alter_type_items, table_name
+            )
+            queries.append(alter_column_types_query)
+        if alter_key_items:
+            logging.info("This operation is not supported by Spark.")
+
+        return queries
 
     def create_query(
         self,
@@ -59,18 +192,21 @@ class MetastoreMigration(DatabaseMigration):
         write_on_entity: bool = None,
     ) -> List[str]:
         """Create a query regarding Metastore.
+
         Args:
             fs_schema: object that contains feature set's schemas.
             table_name: table name.
             db_schema: object that contains the table of a given db schema.
             write_on_entity: boolean flag that indicates if data is being
             loaded into an entity table.
+
         Returns:
             List of queries regarding schemas' changes.
+
         """
         if not db_schema:
             return [self._get_create_table_query(fs_schema, table_name)]
 
         schema_diff = self._get_diff(fs_schema, db_schema)
 
-        return self._get_queries(schema_diff, table_name)
+        return self._get_queries(schema_diff, table_name, write_on_entity)
---
+++ tests/unit/butterfree/migrations/database_migration/test_metastore_migration.py
@@ -0,0 +1,34 @@
+from butterfree.migrations.database_migration import MetastoreMigration
+
+
+class TestMetastoreMigration:
+    def test_queries(self, fs_schema, db_schema):
+        metastore_migration = MetastoreMigration()
+
+        expected_query = [
+            "ALTER TABLE test.table_name ADD IF NOT EXISTS "
+            "columns (new_feature FloatType);",
+            "ALTER TABLE table_name DROP IF EXISTS "
+            "(feature1__avg_over_2_days_rolling_windows None);",
+            "ALTER TABLE table_name ALTER COLUMN "
+            "(feature1__avg_over_1_week_rolling_windows FloatType);",
+        ]
+
+        query = metastore_migration.create_query(fs_schema, "table_name", db_schema)
+
+        assert query, expected_query
+
+    def test_create_table_query(self, fs_schema):
+
+        metastore_migration = MetastoreMigration()
+
+        expected_query = [
+            "CREATE TABLE IF NOT EXISTS  test.table_name "
+            "(id LongType, timestamp TimestampType, new_feature FloatType, "
+            "feature1__avg_over_1_week_rolling_windows FloatType, year INT, "
+            "month INT, day INT) PARTITIONED BY (year, month, day);"
+        ]
+
+        query = metastore_migration.create_query(fs_schema, "table_name")
+
+        assert query, expected_query
