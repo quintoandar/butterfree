7336e89750a594cb7f93fa1f582d4728677c7b19
Mayara Moromisato <44944954+moromimay@users.noreply.github.com>
2021-04-01 14:46:53 -0300

    [MLOP-639] Track logs in S3 (#306)
    * Apply tracking logs and logging config.
    
    * Adjusts in CLI and logging.conf.
    
    * Some adjusts.
    
    * Change version to generate new dev package
    
    * Fix version.
    
    * Apply style.
    
    * Add new assert in the migrate unit test.

--- butterfree/__init__.py
+++ butterfree/__init__.py
@@ -1,1 +1,8 @@
 """Module docstring example, following Google's docstring style."""
+import logging.config
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("."))
+
+logging.config.fileConfig(fname="butterfree/logging.conf")
