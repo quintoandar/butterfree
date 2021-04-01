d73a0a7c9500d0d56a94991b9f716ca87a555d37
Mayara Moromisato <may.alveslima@gmail.com>
2021-04-01 14:24:06 -0300

    Apply style.

--- tests/unit/butterfree/_cli/test_migrate.py
+++ tests/unit/butterfree/_cli/test_migrate.py
@@ -7,13 +7,13 @@ from butterfree.migrations.database_migration import (
 )
 from butterfree.pipelines import FeatureSetPipeline
 
+
 class TestMigrate:
     def test_migrate_success(self, mocker):
         mocker.patch.object(migrate.Migrate, "run")
         all_fs = migrate.migrate("tests/mocks/entities/")
         assert all(isinstance(fs, FeatureSetPipeline) for fs in all_fs)
         assert sorted([fs.feature_set.name for fs in all_fs]) == ["first", "second"]
-
 
     def test_migrate_all_pairs(self, mocker):
         mocker.patch.object(MetastoreMigration, "apply_migration")
@@ -25,11 +25,18 @@ class TestMigrate:
         assert MetastoreMigration.apply_migration.call_count == 2
         assert CassandraMigration.apply_migration.call_count == 2
 
-        metastore_pairs = [call(pipe.feature_set, pipe.sink.writers[0]) for pipe in all_fs]
-        cassandra_pairs = [call(pipe.feature_set, pipe.sink.writers[1]) for pipe in all_fs]
-        MetastoreMigration.apply_migration.assert_has_calls(metastore_pairs, any_order=True)
-        CassandraMigration.apply_migration.assert_has_calls(cassandra_pairs, any_order=True)
-
+        metastore_pairs = [
+            call(pipe.feature_set, pipe.sink.writers[0]) for pipe in all_fs
+        ]
+        cassandra_pairs = [
+            call(pipe.feature_set, pipe.sink.writers[1]) for pipe in all_fs
+        ]
+        MetastoreMigration.apply_migration.assert_has_calls(
+            metastore_pairs, any_order=True
+        )
+        CassandraMigration.apply_migration.assert_has_calls(
+            cassandra_pairs, any_order=True
+        )
 
     # def test_migrate_send_logs(self, mocker):
     #     mocker.patch.object(migrate.Migrate, "run")
