9dc7b83237739f46d76135d9fd8616bd417c99b7
Mayara Moromisato <44944954+moromimay@users.noreply.github.com>
2021-04-06 09:09:46 -0300

    [BUG] Change logging config (#307)
    * Change logging config.
    
    * Some adjusts.
    
    * Remove a code smell.

--- butterfree/__init__.py
+++ butterfree/__init__.py
@@ -1,8 +1,1 @@
 """Module docstring example, following Google's docstring style."""
-import logging.config
-import os
-import sys
-
-sys.path.insert(0, os.path.abspath("."))
-
-logging.config.fileConfig(fname="butterfree/logging.conf")
