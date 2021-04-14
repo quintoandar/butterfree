e6f67e9a5ff42b4987c8d476a8ebf6df6cfa1aac
Mayara Moromisato <44944954+moromimay@users.noreply.github.com>
2021-04-14 18:13:03 -0300

    Fix path validate. (#311)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 __package_name__ = "butterfree"
-__version__ = "1.2.0.dev9"
+__version__ = "1.2.0.dev10"
 __repository_url__ = "https://github.com/quintoandar/butterfree"
 
 with open("requirements.txt") as f:
