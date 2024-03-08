1f7f2c12e02cb704458e4bfdc23a2fa2fb422067
João Albuquerque <joao.albuquerque@quintoandar.com.br>
2024-03-08 10:26:01 -0300

    feat(updating-auto-fs-creation): adding methods to the class as private and add Table dataclass

--- butterfree/automated/feature_set_creation.py
+++ butterfree/automated/feature_set_creation.py
@@ -1,6 +1,9 @@
-from typing import Optional
 import re
+from dataclasses import dataclass
+from typing import List, Optional, Tuple
+
 from pyspark.sql import DataFrame
+
 from butterfree.constants.data_type import DataType
 
 BUTTERFREE_DTYPES = {
@@ -19,87 +22,95 @@ BUTTERFREE_DTYPES = {
 }
 
 
-def get_features_with_regex(sql_query):
-    features = []
-    sql_query = " ".join(sql_query.split())
-    first_pattern = re.compile("[(]?([\w.*]+)[)]?,", re.IGNORECASE)
-    second_pattern = re.compile("(\w+)\s(from)", re.IGNORECASE)
-
-    for pattern in [first_pattern, second_pattern]:
-        matches = pattern.finditer(sql_query)
-        for match in matches:
-            feature = match.group(1)
-
-            if "." in feature:
-                feature = feature.split(".")[1]
-
-            features.append(feature)
-
-    return features
-
-
-def get_data_type(field_name, df):
-    for field in df.schema.jsonValue()["fields"]:
-        if field["name"] == field_name:
-
-            field_type = field["type"]
-
-            if isinstance(field_type, dict):
-
-                field_type_keys = field_type.keys()
-
-                if "type" in field_type_keys and "elementType" in field_type_keys:
-                    return (
-                        "."
-                        + BUTTERFREE_DTYPES[field_type["type"]][
-                            field_type["elementType"]
-                        ]
-                    )
-
-            return "." + BUTTERFREE_DTYPES[field["type"]]
-    return ""
-
-
-def get_tables_with_regex(sql_query):
-    modified_sql_query = sql_query
-
-    tables = []
-    stop_words = ["left", "right", "full outer", "inner", "where", "join", "on"]
-    keywords = ["from", "join"]
-
-    for keyword in keywords:
-        pattern = re.compile(rf"\b{keyword}\s+(\w+\.\w+|\w+)\s+(\w+)", re.IGNORECASE)
-        matches = pattern.finditer(sql_query)
-
-        for match in matches:
-            table_name = match.group(1)
-            id = match.group(2).strip()
-            table = table_name
-
-            if id in stop_words:
-                id = table
-
-            if "." in table_name:
-                database, table = table_name.split(".")
-
-                modified_sql_query = re.sub(
-                    rf"\b{database}\.{table}\b", table, modified_sql_query
-                )
-
-                tables.append({"id": id, "database": database, "table": table})
-            else:
-                modified_sql_query = re.sub(rf"\b{table}\b", table, modified_sql_query)
-                tables.append({"id": id, "database": "TBD", "table": table})
-
-    return tables, modified_sql_query
+@dataclass(frozen=True)
+class Table:
+    id: str
+    database: str
+    name: str
 
 
 class FeatureSetCreation:
     def __init__(self):
         pass
 
-    def get_readers(self, sql_query):
-        tables, modified_sql_query = get_tables_with_regex(sql_query.lower())
+    def _get_features_with_regex(self, sql_query: str):
+        features = []
+        sql_query = " ".join(sql_query.split())
+        first_pattern = re.compile("[(]?([\w.*]+)[)]?,", re.IGNORECASE)
+        second_pattern = re.compile("(\w+)\s(from)", re.IGNORECASE)
+
+        for pattern in [first_pattern, second_pattern]:
+            matches = pattern.finditer(sql_query)
+            for match in matches:
+                feature = match.group(1)
+
+                if "." in feature:
+                    feature = feature.split(".")[1]
+
+                features.append(feature)
+
+        return features
+
+    def _get_data_type(self, field_name, df):
+        for field in df.schema.jsonValue()["fields"]:
+            if field["name"] == field_name:
+
+                field_type = field["type"]
+
+                if isinstance(field_type, dict):
+
+                    field_type_keys = field_type.keys()
+
+                    if "type" in field_type_keys and "elementType" in field_type_keys:
+                        return (
+                            "."
+                            + BUTTERFREE_DTYPES[field_type["type"]][
+                                field_type["elementType"]
+                            ]
+                        )
+
+                return "." + BUTTERFREE_DTYPES[field["type"]]
+        return ""
+
+    def _get_tables_with_regex(self, sql_query: str) -> Tuple[List[dict], str]:
+        modified_sql_query = sql_query
+
+        tables = []
+        stop_words = ["left", "right", "full outer", "inner", "where", "join", "on"]
+        keywords = ["from", "join"]
+
+        for keyword in keywords:
+            pattern = re.compile(
+                rf"\b{keyword}\s+(\w+\.\w+|\w+)\s+(\w+)", re.IGNORECASE
+            )
+            matches = pattern.finditer(sql_query)
+
+            for match in matches:
+                table_name = match.group(1)
+                id = match.group(2).strip()
+                table = table_name
+
+                if id in stop_words:
+                    id = table
+
+                if "." in table_name:
+                    database, table = table_name.split(".")
+
+                    modified_sql_query = re.sub(
+                        rf"\b{database}\.{table}\b", table, modified_sql_query
+                    )
+
+                    tables.append({"id": id, "database": database, "table": table})
+                else:
+                    modified_sql_query = re.sub(
+                        rf"\b{table}\b", table, modified_sql_query
+                    )
+                    tables.append({"id": id, "database": "TBD", "table": table})
+
+        return tables, modified_sql_query
+
+    def get_readers(self, sql_query: str) -> str:
+        tables, modified_sql_query = self._get_tables_with_regex(sql_query.lower())
         readers = []
         for table in tables:
             table_reader_string = f"""
@@ -127,8 +138,8 @@ class FeatureSetCreation:
 
         return final_string
 
-    def get_features(self, sql_query, df: Optional[DataFrame]):
-        features = get_features_with_regex(sql_query)
+    def get_features(self, sql_query: str, df: Optional[DataFrame]) -> str:
+        features = self._get_features_with_regex(sql_query)
         features_formatted = []
         for feature in features:
             description = feature.replace("__", " ").replace("_", " ").capitalize()
@@ -136,7 +147,7 @@ class FeatureSetCreation:
             data_type = "."
 
             if df and isinstance(df, DataFrame):
-                data_type = get_data_type(feature, df)
+                data_type = self._get_data_type(feature, df)
 
             feature_string = f"""
             Feature(
@@ -146,6 +157,7 @@ class FeatureSetCreation:
             ),
             """
             features_formatted.append(feature_string)
+
         final_string = ("features=[" "\t{}" "    ],\n" "),").format(
             "".join(features_formatted)
         )
