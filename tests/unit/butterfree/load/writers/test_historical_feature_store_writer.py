a87954dcdbca6c30ff79e046cff10c82dbe4a9e5
Ralph Filho <ralphrass@gmail.com>
2024-08-15 10:06:41 -0300

    fix: spark

--- tests/unit/butterfree/load/writers/test_historical_feature_store_writer.py
+++ tests/unit/butterfree/load/writers/test_historical_feature_store_writer.py
@@ -161,7 +161,7 @@ class TestHistoricalFeatureStoreWriter:
 
         spark_client._session = (
             SparkSession.builder.master("local[*]")
-            .config("spark.jars.packages", "io.delta:delta-core_2.12:2.0.0")
+            .config("spark.jars.packages", "io.delta:delta-spark_2.12:3.0.0")
             .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
             .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
             .config("spark.delta.logStore.class", "org.apache.spark.sql.delta.storage.S3SingleDriverLogStore")
